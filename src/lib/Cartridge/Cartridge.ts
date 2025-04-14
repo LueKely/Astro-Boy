444b16184b1b141564323bf7f2f4a12916b79a75
Lue Kely <luekely43110@gmail.com>
2025-04-14 18:00:08 +0800

    added static header address lookup table

--- src/lib/Cartridge/Cartridge.ts
+++ src/lib/Cartridge/Cartridge.ts
@@ -1,5 +1,27 @@
+import { CartHeaderAdress } from './types/Catridge_Address_Reader';
+
 /**
  * @description
  * This file contains the code for loading a ROM file.
  **/
-export class GameBoyCatridge {}
+export class GameBoyCatridge {
+	// a cartridge should take the binaries from the buffer array
+	// and "slice" the parts of the array buffer and those sliced parts
+	// will be compared to the data map
+	rawGame: ArrayBuffer;
+	#CatridgeHeader = CartHeaderAdress.Adresses;
+
+	constructor(rawGame: ArrayBuffer) {
+		this.rawGame = rawGame;
+	}
+
+	getCartridgeHeader() {
+		const cartridge = new DataView(this.rawGame);
+		const cartridgeType = cartridge.getUint8(
+			this.#CatridgeHeader.cartridgeType[0]
+		);
+
+		return cartridgeType;
+	}
+	// eg.
+}
---
+++ src/lib/Cartridge/types/Catridge_Address_Reader.ts
@@ -0,0 +1,18 @@
+export class CartHeaderAdress {
+	static Adresses = {
+		entry: [0x100, 0x103],
+		nintendoLogo: [0x104, 0x0133],
+		gameTitle: [0x0134, 0x143],
+		manufactureCode: [0x13f, 0x0142],
+		cgbFlag: [0x0143],
+		newLicenseeCode: [0x0144, 0x0145],
+		cartridgeType: [0x0147],
+		romSize: [0x148],
+		ramSize: [0x149],
+		destinationCode: [0x014a],
+		oldLicenseeCode: [0x14b],
+		maskRomVersionNumber: [0x014c],
+		headerCheckSum: [0x014d],
+		globalCheckSum: [0x014e, 0x014f],
+	};
+}
