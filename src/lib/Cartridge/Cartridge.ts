721645191a90104bbe460dd800487fe7db4959e5
Lue Kely <luekely43110@gmail.com>
2025-04-21 14:06:14 +0800

    Feat: There was a problem with how the subarrays were built so i fixed that

--- src/lib/Cartridge/Cartridge.ts
+++ src/lib/Cartridge/Cartridge.ts
@@ -1,4 +1,4 @@
-import { CartHeaderAdress } from './types/Catridge_Address_Reader';
+import { CartHeaderAdress } from "./types/Catridge_Address_Reader";
 // tonight will infer what these ascii code mean
 /**
  * @description
@@ -6,116 +6,133 @@ import { CartHeaderAdress } from './types/Catridge_Address_Reader';
  **/
 
 export class GameBoyCatridge {
-	// a cartridge should take the binaries from the buffer array
-	// and "slice" the parts of the array buffer and those sliced parts
-	// will be compared to the data map
+  // a cartridge should take the binaries from the buffer array
+  // and "slice" the parts of the array buffer and those sliced parts
+  // will be compared to the data map
 
-	#rawGame: ArrayBuffer;
-	#CartData: DataView;
-	#CartDataToBytes: Uint8Array;
+  #rawGame: ArrayBuffer;
+  #CartData: DataView;
+  #CartDataToBytes: Uint8Array;
 
-	static #ToHex(value: number) {
-		return value.toString(16);
-	}
+  static #ToHex(value: number) {
+    return value.toString(16);
+  }
 
-	static #ReadSubArray(arg: Uint8Array) {
-		const textDecoder = new TextDecoder();
-		return textDecoder.decode(arg);
-	}
+  static #ReadSubArray(arg: Uint8Array) {
+    const textDecoder = new TextDecoder();
+    return textDecoder.decode(arg);
+  }
 
-	constructor(rawGame: ArrayBuffer) {
-		this.#rawGame = rawGame;
-		this.#CartData = new DataView(this.#rawGame);
-		this.#CartDataToBytes = new Uint8Array(this.#CartData.buffer);
-	}
+  static #CipherAscii(args: Uint8Array<ArrayBufferLike>) {
+    const a = String.fromCharCode(args[0]);
+    const b = String.fromCharCode(args[1]);
 
-	getCartridgeHeaderRaw() {
-		const cartridgeType = this.#CartData.getUint8(
-			CartHeaderAdress.Adresses.cartridgeType[0]
-		);
+    return a + b;
+  }
 
-		const cartridgeRomSize = this.#CartData.getUint8(
-			CartHeaderAdress.Adresses.romSize[0]
-		);
+  constructor(rawGame: ArrayBuffer) {
+    this.#rawGame = rawGame;
+    this.#CartData = new DataView(this.#rawGame);
+    this.#CartDataToBytes = new Uint8Array(this.#CartData.buffer);
+  }
 
-		const cartridgeRamSize = this.#CartData.getUint8(
-			CartHeaderAdress.Adresses.ramSize[0]
-		);
+  getCartridgeHeaderRaw() {
+    const cartridgeType = this.#CartData.getUint8(
+      CartHeaderAdress.Adresses.cartridgeType[0],
+    );
 
-		const cartridgeCgbFlag = this.#CartData.getUint8(
-			CartHeaderAdress.Adresses.cgbFlag[0]
-		);
+    const cartridgeRomSize = this.#CartData.getUint8(
+      CartHeaderAdress.Adresses.romSize[0],
+    );
 
-		const cartridgeDestinationCode = this.#CartData.getUint8(
-			CartHeaderAdress.Adresses.destinationCode[0]
-		);
+    const cartridgeRamSize = this.#CartData.getUint8(
+      CartHeaderAdress.Adresses.ramSize[0],
+    );
 
-		const cartridgeOldLicenseeCode = this.#CartData.getUint8(
-			CartHeaderAdress.Adresses.oldLicenseeCode[0]
-		);
+    const cartridgeCgbFlag = this.#CartData.getUint8(
+      CartHeaderAdress.Adresses.cgbFlag[0],
+    );
 
-		const cartridgeMaskRomVersionNumber = this.#CartData.getUint8(
-			CartHeaderAdress.Adresses.maskRomVersionNumber[0]
-		);
+    const cartridgeDestinationCode = this.#CartData.getUint8(
+      CartHeaderAdress.Adresses.destinationCode[0],
+    );
 
-		const cartridgeCheckSum = this.#CartData.getUint8(
-			CartHeaderAdress.Adresses.headerCheckSum[0]
-		);
+    const cartridgeOldLicenseeCode = this.#CartData.getUint8(
+      CartHeaderAdress.Adresses.oldLicenseeCode[0],
+    );
 
-		const cartridgeTitle = this.#CartDataToBytes.subarray(
-			CartHeaderAdress.Adresses.gameTitle[0],
-			CartHeaderAdress.Adresses.gameTitle[1]
-		);
+    const cartridgeMaskRomVersionNumber = this.#CartData.getUint8(
+      CartHeaderAdress.Adresses.maskRomVersionNumber[0],
+    );
 
-		const entryPoint = this.#CartDataToBytes.subarray(
-			CartHeaderAdress.Adresses.entry[0],
-			CartHeaderAdress.Adresses.entry[1]
-		);
+    const cartridgeCheckSum = this.#CartData.getUint8(
+      CartHeaderAdress.Adresses.headerCheckSum[0],
+    );
 
-		const newLicenseeCode = this.#CartDataToBytes.subarray(
-			CartHeaderAdress.Adresses.newLicenseeCode[0],
-			CartHeaderAdress.Adresses.newLicenseeCode[1]
-		);
-		// as pandev implies, the manufacturer code has no know purpose
-		const manufactureCode = this.#CartDataToBytes.subarray(
-			CartHeaderAdress.Adresses.manufactureCode[0],
-			CartHeaderAdress.Adresses.manufactureCode[1]
-		);
+    const cartridgeTitle = this.#CartDataToBytes.subarray(
+      CartHeaderAdress.Adresses.gameTitle[0],
+      CartHeaderAdress.Adresses.gameTitle[1] + 0x01,
+    );
 
-		const globalCheckSum = this.#CartDataToBytes.subarray(
-			CartHeaderAdress.Adresses.globalCheckSum[0],
-			CartHeaderAdress.Adresses.globalCheckSum[1]
-		);
+    const entryPoint = this.#CartDataToBytes.subarray(
+      CartHeaderAdress.Adresses.entry[0],
+      CartHeaderAdress.Adresses.entry[1] + 0x01,
+    );
 
-		return {
-			type: GameBoyCatridge.#ToHex(cartridgeType),
-			romSize: GameBoyCatridge.#ToHex(cartridgeRomSize),
-			ramSize: GameBoyCatridge.#ToHex(cartridgeRamSize),
-			cgbFlag: GameBoyCatridge.#ToHex(cartridgeCgbFlag),
-			destinationCode: GameBoyCatridge.#ToHex(cartridgeDestinationCode),
-			oldLicenseeCode: GameBoyCatridge.#ToHex(cartridgeOldLicenseeCode),
-			maskRomVersionNumber: GameBoyCatridge.#ToHex(
-				cartridgeMaskRomVersionNumber
-			),
-			checkSum: GameBoyCatridge.#ToHex(cartridgeCheckSum),
-			title: GameBoyCatridge.#ReadSubArray(cartridgeTitle),
-			entryPoint: entryPoint,
-			newLicenseeCode: newLicenseeCode,
-			manufactureCode: manufactureCode,
-			globalCheckSum: globalCheckSum,
-		};
-	}
+    const newLicenseeCode = this.#CartDataToBytes.subarray(
+      CartHeaderAdress.Adresses.newLicenseeCode[0],
+      CartHeaderAdress.Adresses.newLicenseeCode[1] + 0x01,
+    );
+    // as pandev implies, the manufacturer code has no know purpose
+    const manufactureCode = this.#CartDataToBytes.subarray(
+      CartHeaderAdress.Adresses.manufactureCode[0],
+      CartHeaderAdress.Adresses.manufactureCode[1] + 0x01,
+    );
 
-	inferCartridgeHeader() {
-		const header = this.getCartridgeHeaderRaw();
+    const globalCheckSum = this.#CartDataToBytes.subarray(
+      CartHeaderAdress.Adresses.globalCheckSum[0],
+      CartHeaderAdress.Adresses.globalCheckSum[1] + 0x01,
+    );
 
-		return {
-			type: CartHeaderAdress.CartridgeType.get(Number(header.type)),
-			romSize: CartHeaderAdress.RomSize.get(Number(header.romSize)),
-			ramSize: CartHeaderAdress.RamSize.get(Number(header.ramSize)),
-			// desciper the the title here instead in the raw variable
-		};
-	}
+    return {
+      type: GameBoyCatridge.#ToHex(cartridgeType),
+      romSize: GameBoyCatridge.#ToHex(cartridgeRomSize),
+      ramSize: GameBoyCatridge.#ToHex(cartridgeRamSize),
+      cgbFlag: GameBoyCatridge.#ToHex(cartridgeCgbFlag),
+      destinationCode: GameBoyCatridge.#ToHex(cartridgeDestinationCode),
+      oldLicenseeCode: GameBoyCatridge.#ToHex(cartridgeOldLicenseeCode),
+      maskRomVersionNumber: GameBoyCatridge.#ToHex(
+        cartridgeMaskRomVersionNumber,
+      ),
+      checkSum: GameBoyCatridge.#ToHex(cartridgeCheckSum),
+      title: cartridgeTitle,
+      entryPoint: entryPoint,
+      // this part should be an ASCII code
+      newLicenseeCode: newLicenseeCode,
+      manufactureCode: manufactureCode,
+      globalCheckSum: globalCheckSum,
+    };
+  }
 
-	// eg.
+  inferCartridgeHeader() {
+    const header = this.getCartridgeHeaderRaw();
+
+    return {
+      type: CartHeaderAdress.CartridgeType.get(Number(header.type)),
+      romSize: CartHeaderAdress.RomSize.get(Number(header.romSize)),
+      ramSize: CartHeaderAdress.RamSize.get(Number(header.ramSize)),
+      destination: CartHeaderAdress.DestinationCode.get(
+        Number(header.destinationCode),
+      ),
+      title: GameBoyCatridge.#ReadSubArray(header.title),
+      oldLicenseeCode: CartHeaderAdress.OldLicenseeCode.get(
+        Number("0x" + header.oldLicenseeCode),
+      ),
+      newLicenseeCode: CartHeaderAdress.NewLicenseeCode.get(
+        GameBoyCatridge.#CipherAscii(header.newLicenseeCode),
+      ),
+    };
+  }
+
+  // eg.
 }
