e6ea7ce2b1bdaacdfc8e05d57b45c4187396ec4c
Lue Kely <luekely43110@gmail.com>
2025-05-03 11:09:14 +0800

    finished flag register!

--- src/lib/Cpu/CPU_Flag_Register.ts
+++ src/lib/Cpu/CPU_Flag_Register.ts
@@ -3,21 +3,47 @@ export class CPU_Flag_Register {
   static #subtractionFlagPosition = 6;
   static #halfCarryFlagPosition = 5;
   static #carryFlagPosition = 4;
-  register: number = 0;
-  #flag = { Z: 0, N: 0, H: 0, CY: 0 };
-
-  constuctor(register: number) {
-    this.register = register;
-  }
+  #register: number = 0;
   // 0000 0000
+  //   #flag = { Z: 0, N: 0, H: 0, CY: 0 };
+  constuctor(register: number) {
+    this.#register = register;
+  }
 
-  // convert to carry flag and back to 8 bits again
-  //   get and set all flags
+  getZFlag() {
+    return this.#register >> CPU_Flag_Register.#zeroFlagPosition;
+  }
+
+  getNFlag() {
+    return this.#register >> CPU_Flag_Register.#subtractionFlagPosition;
+  }
+
+  getHFlag() {
+    return this.#register >> CPU_Flag_Register.#halfCarryFlagPosition;
+  }
+
+  getCYFlag() {
+    return this.#register >> CPU_Flag_Register.#carryFlagPosition;
+  }
+
+  getFRegister() {
+    return this.#register;
+  }
 
   setZFlag(output: number) {
-    // check if the output of the operation
+    // check if the output of the operation is zero
+    //  if it is zero mark the flag as 1
+    // return add 128
+    this.#register |= output === 0 ? 0b1000000 : 0;
   }
-  setNFlag(condition: boolean) {}
-  setHFlag(condition: boolean) {}
-  setCYFlag(condition: boolean) {}
+  //   when raising the subtraction flag, it will only be raised when the last operation is subtraction
+  setNFlag(output: number) {
+    this.#register |= output >> CPU_Flag_Register.#subtractionFlagPosition;
+  }
+  setHFlag(output: number) {
+    this.#register |= output >> CPU_Flag_Register.#halfCarryFlagPosition;
+  }
+  setCYFlag(output: number) {
+    this.#register |= output >> CPU_Flag_Register.#carryFlagPosition;
+  }
 }
