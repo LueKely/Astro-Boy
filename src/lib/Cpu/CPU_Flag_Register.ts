4881598cc2a265672460e71461e500ff7501c2db
Lue Kely <luekely43110@gmail.com>
2025-05-07 20:26:30 +0800

    Test: Added Tests for Registers B-E

--- src/lib/Cpu/CPU_Flag_Register.ts
+++ src/lib/Cpu/CPU_Flag_Register.ts
@@ -1,49 +1,55 @@
 export class CPU_Flag_Register {
-  static #zeroFlagPosition = 7;
-  static #subtractionFlagPosition = 6;
-  static #halfCarryFlagPosition = 5;
-  static #carryFlagPosition = 4;
-  #register: number = 0;
-  // 0000 0000
-  //   #flag = { Z: 0, N: 0, H: 0, CY: 0 };
-  constructor(register: number) {
-    this.#register = register;
-  }
+	static #zeroFlagPosition = 7;
+	static #subtractionFlagPosition = 6;
+	static #halfCarryFlagPosition = 5;
+	static #carryFlagPosition = 4;
+	#register: number = 0;
+	// 0000 0000
+	//   #flag = { Z: 0, N: 0, H: 0, CY: 0 };
 
-  getZFlag() {
-    return this.#register >> CPU_Flag_Register.#zeroFlagPosition;
-  }
+	//  bits 0-3 are not needed for used in the Flag
 
-  getNFlag() {
-    return this.#register >> CPU_Flag_Register.#subtractionFlagPosition;
-  }
+	// note to lue
+	// this flag should only be handled by the instruction set
+	// not the actual class itself
+	constructor(register: number) {
+		this.#register = register;
+	}
 
-  getHFlag() {
-    return this.#register >> CPU_Flag_Register.#halfCarryFlagPosition;
-  }
+	getZFlag() {
+		return this.#register >> CPU_Flag_Register.#zeroFlagPosition;
+	}
 
-  getCYFlag() {
-    return this.#register >> CPU_Flag_Register.#carryFlagPosition;
-  }
+	getNFlag() {
+		return this.#register >> CPU_Flag_Register.#subtractionFlagPosition;
+	}
 
-  getFRegister() {
-    return this.#register;
-  }
+	getHFlag() {
+		return this.#register >> CPU_Flag_Register.#halfCarryFlagPosition;
+	}
 
-  setZFlag(output: number) {
-    // check if the output of the operation is zero
-    //  if it is zero mark the flag as 1
-    // return add 128
-    this.#register |= output === 0 ? 0b1000000 : 0;
-  }
-  //   when raising the subtraction flag, it will only be raised when the last operation is subtraction
-  setNFlag(output: number) {
-    this.#register |= output >> CPU_Flag_Register.#subtractionFlagPosition;
-  }
-  setHFlag(output: number) {
-    this.#register |= output >> CPU_Flag_Register.#halfCarryFlagPosition;
-  }
-  setCYFlag(output: number) {
-    this.#register |= output >> CPU_Flag_Register.#carryFlagPosition;
-  }
+	getCYFlag() {
+		return this.#register >> CPU_Flag_Register.#carryFlagPosition;
+	}
+
+	getFRegister() {
+		return this.#register;
+	}
+
+	setZFlag(output: number) {
+		// check if the output of the operation is zero
+		//  if it is zero mark the flag as 1
+		// return add 128
+		this.#register |= output === 0 ? 0b1000000 : 0;
+	}
+	//   when raising the subtraction flag, it will only be raised when the last operation is subtraction
+	setNFlag(output: number) {
+		this.#register |= output >> CPU_Flag_Register.#subtractionFlagPosition;
+	}
+	setHFlag(output: number) {
+		this.#register |= output >> CPU_Flag_Register.#halfCarryFlagPosition;
+	}
+	setCYFlag(output: number) {
+		this.#register |= output >> CPU_Flag_Register.#carryFlagPosition;
+	}
 }
--- src/lib/Cpu/tests/CPU_Registers.test.ts
+++ src/lib/Cpu/tests/CPU_Registers.test.ts
@@ -1,14 +1,77 @@
-import { expect, test } from "vitest";
-import { CPU_Registers } from "../CPU_Registers";
+import { describe, expect, test } from 'vitest';
+import { CPU_Registers } from '../CPU_Registers';
 
 const Register = new CPU_Registers();
 
-Register.register.B = 10;
-Register.register.C = 10;
+describe('Read and Write inside the registers', () => {
+	test.each([
+		Register.register.B,
+		Register.register.C,
+		Register.register.D,
+		Register.register.E,
+	])('Each Register should read and right', (address) => {
+		address = 5;
+		expect(address).equal(5);
+	});
+});
 
-test.each([
-  Register.register.B,
-  Register.register.C,
-  Register.register.D,
-  Register.register.E,
-])("Each Register should read and right", (address) => {});
+describe('Read And Write in BC Register', () => {
+	test('Should Get BC', () => {
+		Register.register.B = 0b0001;
+		Register.register.C = 0b0001;
+		expect(Register.getBC()).toBe(0b000100000001);
+	});
+
+	test('Should Set BC', () => {
+		Register.setBC(4369);
+		expect(Register.getBC()).toBe(4369);
+		expect(Register.register.B).toBe(0b00010001);
+		expect(Register.register.C).toBe(0b00010001);
+	});
+
+	test('Set BC value to 69 Excpect B to be 0 & C to be 69', () => {
+		Register.setBC(69);
+		expect(Register.getBC()).toBe(69);
+		expect(Register.register.B).toBe(0);
+		expect(Register.register.C).toBe(69);
+	});
+
+	test('Set B to 0 and C to 69 Expect BC to be 69', () => {
+		Register.register.B = 0;
+		Register.register.C = 69;
+		expect(Register.getBC()).toBe(69);
+	});
+});
+
+describe('Read And Write in DE Register', () => {
+	test('Should Get DE', () => {
+		Register.register.D = 0b0001;
+		Register.register.E = 0b0001;
+		expect(Register.getDE()).toBe(0b000100000001);
+	});
+
+	test('Should Set DE', () => {
+		Register.setDE(4369);
+		expect(Register.getDE()).toBe(4369);
+		expect(Register.register.D).toBe(0b00010001);
+		expect(Register.register.E).toBe(0b00010001);
+	});
+
+	test('Set DE value to 69 Excpect D to be 0 & E to be 69', () => {
+		Register.setDE(69);
+		expect(Register.getDE()).toBe(69);
+		expect(Register.register.D).toBe(0);
+		expect(Register.register.E).toBe(69);
+	});
+
+	test('Set D to 0 and E to 69 Expect DE to be 69', () => {
+		Register.register.D = 0;
+		Register.register.E = 69;
+		expect(Register.getDE()).toBe(69);
+	});
+});
+
+describe('Flag Register', () => {
+	test('Individual Flags');
+	test('Combined Flags');
+});
