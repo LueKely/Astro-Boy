78fd4872780d473350053388ee18dd458abc8614
Lue Kely <luekely43110@gmail.com>
2025-05-07 20:25:50 +0800

    Fix: Adjusted the Set for registration

--- src/lib/Cpu/CPU_Registers.ts
+++ src/lib/Cpu/CPU_Registers.ts
@@ -1,5 +1,5 @@
-import { CPU_Flag_Register } from "./CPU_Flag_Register";
-import type { IRegisters } from "./types/CPU_Register_Types";
+import { CPU_Flag_Register } from './CPU_Flag_Register';
+import type { IRegisters } from './types/CPU_Register_Types';
 
 /*
  * The CPU REGISTER
@@ -16,47 +16,47 @@ import type { IRegisters } from "./types/CPU_Register_Types";
  */
 
 export class CPU_Registers {
-  register: IRegisters = {
-    A: 0,
-    B: 0,
-    C: 0,
-    D: 0,
-    E: 0,
-    F: new CPU_Flag_Register(0),
-    G: 0,
-    H: 0,
-    L: 0,
-    PC: 0, // 16 bit register
-  };
+	register: IRegisters = {
+		A: 0,
+		B: 0,
+		C: 0,
+		D: 0,
+		E: 0,
+		F: new CPU_Flag_Register(0),
+		G: 0,
+		H: 0,
+		L: 0,
+		PC: 0, // 16 bit register
+	};
 
-  constructor() {}
-  // this is a but suspicious i might check on this later
-  getAF() {
-    return (this.register.A << 8) | this.register.F.getFRegister();
-  }
+	constructor() {}
+	// this is a but suspicious i might check on this later
+	getAF() {
+		return (this.register.A << 8) | this.register.F.getFRegister();
+	}
 
-  getBC() {
-    return (this.register.B << 8) | this.register.C;
-  }
+	getBC() {
+		return (this.register.B << 8) | this.register.C;
+	}
 
-  setBC(value: number) {
-    this.register.B = (value & 0xff0) >> 8;
-    this.register.C = value & 0xff;
-  }
-  getDE() {
-    return (this.register.D << 8) | this.register.E;
-  }
+	setBC(value: number) {
+		this.register.B = (value & 0xff00) >> 8;
+		this.register.C = value & 0xff;
+	}
+	getDE() {
+		return (this.register.D << 8) | this.register.E;
+	}
 
-  setDE(value: number) {
-    this.register.D = (value & 0xff0) >> 8;
-    this.register.E = value & 0xff;
-  }
+	setDE(value: number) {
+		this.register.D = (value & 0xff00) >> 8;
+		this.register.E = value & 0xff;
+	}
 
-  getHL() {
-    return (this.register.H << 8) | this.register.L;
-  }
-  setHL(value: number) {
-    this.register.H = (value & 0xff0) >> 8;
-    this.register.L = value & 0xff;
-  }
+	getHL() {
+		return (this.register.H << 8) | this.register.L;
+	}
+	setHL(value: number) {
+		this.register.H = (value & 0xff00) >> 8;
+		this.register.L = value & 0xff;
+	}
 }
