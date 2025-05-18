f429cda77a66bdd1219cb9e5251f1becac28f56c
Lue Kely <luekely43110@gmail.com>
2025-05-18 20:09:35 +0800

    this is all for now

--- src/lib/Cpu/instructions/LD_Instructions.ts
+++ src/lib/Cpu/instructions/LD_Instructions.ts
@@ -1,5 +1,7 @@
-// LD r, r'
+// note:
+// [XX] means it is a pointer pointing to the address bus
 
+// LD r, r'
 import type { CPU_Registers } from '../CPU_Registers';
 
 function LDR8R8(r8l: number, r8r: number) {
@@ -18,13 +20,15 @@ function LDHLN8(HL: CPU_Registers, n8: number) {
 	HL.setHL(n8);
 }
 
-function LDR8HL(r8: number, HL: CPU_Registers) {
-	r8 = HL.getHL();
+// LD R8, [HL]
+function LDR8HL(r8: number, HL: CPU_Registers, memAdd: Uint8Array) {
+	r8 = memAdd[HL.getHL()];
 }
-
+// LD[n16],A
 function LDN16A(n16: number, a: number) {
 	n16 = a;
 }
+// LDH [C], A
 function LDHCA(c: Uint8Array, a: number) {
 	c[0xff00 + 0xc] = a;
 }
@@ -36,4 +40,25 @@ function LDHCA(c: Uint8Array, a: number) {
 function LDAPR16(a: number, pr16: number) {
 	a = pr16;
 }
-export { LDR16N16, LDR8N8, LDR8R8, LDHLN8, LDR8HL, LDHCA, LDN16A };
+// LD [HLI], A
+function LDHLIA(HLPointer: number, HL: CPU_Registers, a: number) {
+	HLPointer = a;
+	HL.setHL(HL.getHL() + 1);
+}
+// LD [HLD], A
+function LDHLDA(HLPointer: number, HL: CPU_Registers, a: number) {
+	HLPointer = a;
+	HL.setHL(HL.getHL() - 1);
+}
+export {
+	LDR16N16,
+	LDR8N8,
+	LDR8R8,
+	LDHLN8,
+	LDR8HL,
+	LDHCA,
+	LDN16A,
+	LDAPR16,
+	LDHLIA,
+	LDHLDA,
+};
