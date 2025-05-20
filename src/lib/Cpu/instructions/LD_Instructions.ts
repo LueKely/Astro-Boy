77306282f308807d4df9ac10f9103c78c4488975
Lue Kely <luekely43110@gmail.com>
2025-05-20 09:44:19 +0800

    init: LD instruction test

--- src/lib/Cpu/instructions/LD_Instructions.ts
+++ src/lib/Cpu/instructions/LD_Instructions.ts
@@ -1,51 +1,51 @@
-import type { CPU_Registers_Address } from "../CPU_Registers_Address";
+import type { Cpu_Register, Cpu_Register_16Bit } from "../CPU_Register";
 // note:
 // [XX] means it is a pointer pointing to the address bus
 // bro all of this shit doesn't work hahaha
 // LD R8, R8
 // LD r, r'
-function LDR8R8(r8l: number, r8r: number) {
-  r8l = r8r;
+function LDR8R8(r8l: Cpu_Register, r8r: Cpu_Register) {
+  r8l.setRegister(r8r.getRegister());
 }
 // LD R8, N8
-function LDR8N8(r8: number, n8: number) {
-  r8 = n8;
+function LDR8N8(r8: Cpu_Register, n8: number) {
+  r8.setRegister(n8);
 }
 // LD R16, N16
-function LDR16N16(r16: (value: number) => void, n16: number) {
-  r16(n16);
+function LDR16N16(r16: Cpu_Register_16Bit, n16: number) {
+  r16.setRegister(n16);
 }
 
 // LD [HL], R8
-function LDHLR8(HL: CPU_Registers_Address, r8: number, memAdd: Uint8Array) {
-  memAdd[HL.getHL()] = r8;
+function LDHLR8(HL: Cpu_Register_16Bit, r8: Cpu_Register, memAdd: Uint8Array) {
+  memAdd[HL.getRegister()] = r8.getRegister();
 }
 
 // LD [HL], N8
-function LDHLN8(HL: CPU_Registers_Address, n8: number, memAdd: Uint8Array) {
-  memAdd[HL.getHL()] = n8;
+function LDHLN8(HL: Cpu_Register_16Bit, n8: number, memAdd: Uint8Array) {
+  memAdd[HL.getRegister()] = n8;
 }
 
 // LD R8, [HL]
-function LDR8HL(r8: number, HL: CPU_Registers_Address, memAdd: Uint8Array) {
-  r8 = memAdd[HL.getHL()];
+function LDR8HL(r8: Cpu_Register, HL: Cpu_Register_16Bit, memAdd: Uint8Array) {
+  r8.setRegister(memAdd[HL.getRegister()]);
 }
 // LD[n16],A
-function LDN16A(n16: number, a: number) {
-  n16 = a;
+function LDN16A(n16: number, a: Cpu_Register, memAdd: Uint8Array) {
+  memAdd[n16] = a.getRegister();
 }
 // LDH [n16], A
-function LDHN16A(n16: number, a: number, pos: number) {
-  if (pos > 0xff00 && pos < 0xffff) {
-    n16 = a;
+function LDHN16A(n16: number, a: Cpu_Register, memAdd: Uint8Array) {
+  if (n16 >= 0xff00 && 16 <= 0xffff) {
+    memAdd[n16] = a.getRegister();
   } else {
     throw new Error("pos is out of scope");
   }
 }
 
 // LDH [C], A
-function LDHCA(c: Uint8Array, a: number) {
-  c[0xff00 + 0xc] = a;
+function LDHCA(memAdd: Uint8Array, a: Cpu_Register, c: Cpu_Register) {
+  memAdd[0xff00 + c.getRegister()] = a.getRegister();
 }
 
 // this is LD A,[R16]
@@ -53,21 +53,21 @@ function LDHCA(c: Uint8Array, a: number) {
 // let R16 = CPU.Registers.register.getBC
 // let [R16] = memory(R16)
 // LDAPR16
-function LDAPR16(a: number, pr16: number) {
-  a = pr16;
+function LDAPR16(a: Cpu_Register, r16: Cpu_Register_16Bit, memAdd: Uint8Array) {
+  a.setRegister(memAdd[r16.getRegister()]);
 }
 
 // LD A, [N16]
-function LDAPN16(a: number, n16: number) {
-  a = n16;
+function LDAPN16(a: Cpu_Register, n16: number, memAdd: Uint8Array) {
+  a.setRegister(memAdd[n16]);
 }
 
 // LDH A, [N16]
-function LDHAN16(n16: number, a: number, pos: number) {
-  if (pos > 0xff00 && pos < 0xffff) {
-    a = n16;
+function LDHAN16(n16: number, a: Cpu_Register, memAdd: Uint8Array) {
+  if (n16 >= 0xff00 && n16 <= 0xffff) {
+    a.setRegister(memAdd[n16]);
   } else {
-    throw new Error("pos is out of scope");
+    throw new Error("pointer is out of scope");
   }
 }
 
@@ -77,27 +77,26 @@ function LDHAC(c: number, a: number, address: Uint8Array) {
 }
 
 // LD [HLI], A
-function LDHLIA(HLPointer: number, HL: CPU_Registers_Address, a: number) {
-  HLPointer = a;
-  HL.setHL(HL.getHL() + 1);
+function LDHLIA(memAdd: Uint8Array, HL: Cpu_Register_16Bit, a: Cpu_Register) {
+  memAdd[HL.getRegister()] = a.getRegister();
+  HL.setRegister(HL.getRegister() + 1);
 }
 // LD [HLD], A
-function LDHLDA(HLPointer: number, HL: CPU_Registers_Address, a: number) {
-  HLPointer = a;
-  HL.setHL(HL.getHL() - 1);
+function LDHLDA(memAdd: Uint8Array, HL: Cpu_Register_16Bit, a: Cpu_Register) {
+  memAdd[HL.getRegister()] = a.getRegister();
+  HL.setRegister(HL.getRegister() - 1);
 }
 
 // LD A,[HLD]
-function LDAHLD(a: number, HL: CPU_Registers_Address, address: Uint8Array) {
-  a = address[HL.getHL()];
-
-  HL.setHL(HL.getHL() - 1);
+function LDAHLD(a: Cpu_Register, HL: Cpu_Register_16Bit, memAdd: Uint8Array) {
+  a.setRegister(memAdd[HL.getRegister()]);
+  HL.setRegister(HL.getRegister() - 1);
 }
 
 // LD A, [HLI]
-function LDAHLI(a: number, HL: CPU_Registers_Address, address: Uint8Array) {
-  a = address[HL.getHL()];
-  HL.setHL(HL.getHL() + 1);
+function LDAHLI(a: Cpu_Register, HL: Cpu_Register_16Bit, memAdd: Uint8Array) {
+  a.setRegister(memAdd[HL.getRegister()]);
+  HL.setRegister(HL.getRegister() + 1);
 }
 
 export {
---
+++ src/lib/Cpu/tests/Cpu_Registers_Group.test.ts
@@ -0,0 +1,132 @@
+import { describe, expect, test } from "vitest";
+import { CPU_Registers_Group } from "../CPU_Registers_Group";
+
+const Register = new CPU_Registers_Group();
+
+describe("Read and Write inside the registers", () => {
+  test.each([
+    Register.register.B,
+    Register.register.C,
+    Register.register.D,
+    Register.register.E,
+  ])("Each Register should read and right", (address) => {
+    address.setRegister(5);
+    expect(address.getRegister()).equal(5);
+  });
+});
+
+describe("Read And Write in the 16bit Registers", () => {
+  const testCase = [
+    {
+      r16: Register.register16Bit.AF,
+      r8a: Register.register.A,
+      r8b: Register.register.F,
+    },
+    {
+      r16: Register.register16Bit.BC,
+      r8a: Register.register.B,
+      r8b: Register.register.C,
+    },
+    {
+      r16: Register.register16Bit.DE,
+      r8a: Register.register.D,
+      r8b: Register.register.E,
+    },
+    {
+      r16: Register.register16Bit.HL,
+      r8a: Register.register.H,
+      r8b: Register.register.L,
+    },
+  ];
+
+  test.each(testCase)("Should Get r16", (registers) => {
+    registers.r8a.setRegister(0b0001);
+    registers.r8b.setRegister(0b0001);
+    expect(registers.r16.getRegister()).toBe(0b000100000001);
+  });
+
+  test.each(testCase)("Should Set r16", (registers) => {
+    registers.r16.setRegister(4369);
+    expect(registers.r16.getRegister()).toBe(4369);
+    expect(registers.r8a.getRegister()).toBe(0b00010001);
+    expect(registers.r8b.getRegister()).toBe(0b00010001);
+  });
+
+  test.each(testCase)(
+    "Set r16 value to 69 Excpect r8a to be 0 & r8b to be 69",
+    (registers) => {
+      registers.r16.setRegister(69);
+      expect(registers.r16.getRegister()).toBe(69);
+      expect(registers.r8a.getRegister()).toBe(0);
+      expect(registers.r8b.getRegister()).toBe(69);
+    }
+  );
+
+  test.each(testCase)(
+    "Set r8a to 0 and r8b to 69 Expect r16 to be 69",
+    (registers) => {
+      registers.r8a.setRegister(0);
+      registers.r8b.setRegister(69);
+      expect(registers.r16.getRegister()).toBe(69);
+    }
+  );
+});
+
+describe("Flag Register", () => {
+  test("Individual Flags", () => {
+    // clear the register
+    Register.register.F.setRegister(0);
+
+    Register.register.F.setZFlag();
+    Register.register.F.setCYFlag();
+    Register.register.F.setHFlag();
+    Register.register.F.setNFlag();
+
+    expect(Register.register.F.getZFlag()).not.toBe(0);
+    expect(Register.register.F.getNFlag()).not.toBe(0);
+    expect(Register.register.F.getHFlag()).not.toBe(0);
+    expect(Register.register.F.getCYFlag()).not.toBe(0);
+    expect(Register.register.F.getFRegister()).toBe(0b1111_0000);
+
+    Register.register.F.setZFlag();
+    Register.register.F.setCYFlag();
+    Register.register.F.setHFlag();
+    Register.register.F.setNFlag();
+
+    expect(Register.register.F.getHFlag()).toBe(0);
+    expect(Register.register.F.getNFlag()).toBe(0);
+    expect(Register.register.F.getCYFlag()).toBe(0);
+    expect(Register.register.F.getFRegister()).toBe(0b0);
+  });
+
+  test("Combined Flags", () => {
+    // clear the register
+    Register.register.F.setRegister(0);
+
+    Register.register.F.setCYFlag();
+    Register.register.F.setHFlag();
+    expect(Register.register.F.getFRegister()).toBe(0b0011_0000);
+
+    Register.register.F.setCYFlag();
+    Register.register.F.setHFlag();
+    expect(Register.register.F.getFRegister()).toBe(0b0);
+
+    Register.register.F.setCYFlag();
+    Register.register.F.setZFlag();
+    expect(Register.register.F.getFRegister()).toBe(0b1001_0000);
+
+    Register.register.F.setCYFlag();
+    Register.register.F.setZFlag();
+    expect(Register.register.F.getFRegister()).toBe(0b0);
+
+    Register.register.F.setCYFlag();
+    Register.register.F.setZFlag();
+    Register.register.F.setHFlag();
+    expect(Register.register.F.getFRegister()).toBe(0b1011_0000);
+
+    Register.register.F.setCYFlag();
+    Register.register.F.setZFlag();
+    Register.register.F.setHFlag();
+    expect(Register.register.F.getFRegister()).toBe(0b0);
+  });
+});
