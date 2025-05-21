2b3e73ccd01f559341f3d9bb897b3658e1c93f5f
Lue Kely <luekely43110@gmail.com>
2025-05-21 21:36:07 +0800

    added LD[HL], R8 Tests

--- src/lib/Cpu/__tests__/LD_Insturctions.test.ts
+++ src/lib/Cpu/__tests__/LD_Insturctions.test.ts
@@ -1,49 +1,75 @@
-import { describe, expect, test } from "vitest";
-import { LDR16N16, LDR8N8, LDR8R8 } from "../instructions/LD_Instructions";
-import { CPU_Registers_Group } from "../CPU_Registers_Group";
+import { describe, expect, test } from 'vitest';
+import {
+	LDHLR8,
+	LDR16N16,
+	LDR8N8,
+	LDR8R8,
+} from '../instructions/LD_Instructions';
+import { CPU_Registers_Group } from '../CPU_Registers_Group';
 
-describe("This will test the LD r8, r8 function", () => {
-  const CpuRegisterGroup = new CPU_Registers_Group();
+describe('This will test the LD r8, r8 function', () => {
+	const CpuRegisterGroup = new CPU_Registers_Group();
 
-  for (const [keyA, valueA] of Object.entries(CpuRegisterGroup.register)) {
-    for (const [keyB, valueB] of Object.entries(CpuRegisterGroup.register)) {
-      // skip registers if they have the same key
-      if (keyA == keyB) continue;
+	for (const [keyA, valueA] of Object.entries(CpuRegisterGroup.register)) {
+		for (const [keyB, valueB] of Object.entries(CpuRegisterGroup.register)) {
+			// skip registers if they have the same key
+			if (keyA == keyB) continue;
 
-      test(`Tests register ${keyA} and ${keyB} `, () => {
-        valueB.setRegister(0xff);
-        LDR8R8(valueA, valueB);
-        expect(valueA.getRegister()).toBe(0xff);
-        expect(valueB.getRegister()).toBe(0xff);
-      });
-      valueA.setRegister(0);
-      valueB.setRegister(0);
-    }
-  }
+			test(`Tests register ${keyA} and ${keyB} `, () => {
+				valueB.setRegister(0xff);
+				LDR8R8(valueA, valueB);
+				expect(valueA.getRegister()).toBe(0xff);
+				expect(valueB.getRegister()).toBe(0xff);
+			});
+			valueA.setRegister(0);
+			valueB.setRegister(0);
+		}
+	}
 });
 
-describe("This will test the LD r8, n8 function", () => {
-  const CpuRegisterGroup = new CPU_Registers_Group();
+describe('This will test the LD r8, n8 function', () => {
+	const CpuRegisterGroup = new CPU_Registers_Group();
 
-  for (const [key, value] of Object.entries(CpuRegisterGroup.register)) {
-    // skip registers if they have the same key
+	for (const [key, value] of Object.entries(CpuRegisterGroup.register)) {
+		// skip registers if they have the same key
 
-    test(`Tests register  ${key} to get the value 0xf0 `, () => {
-      LDR8N8(value, 0xf0);
-      expect(value.getRegister()).toBe(0xf0);
-    });
-  }
+		test(`Tests register  ${key} to get the value 0xf0 `, () => {
+			LDR8N8(value, 0xf0);
+			expect(value.getRegister()).toBe(0xf0);
+		});
+	}
 });
 
-describe("This will test the LD R16, N16 function", () => {
-  const CpuRegisterGroup = new CPU_Registers_Group();
+describe('This will test the LD R16, N16 function', () => {
+	const CpuRegisterGroup = new CPU_Registers_Group();
 
-  for (const [key, value] of Object.entries(CpuRegisterGroup.register16Bit)) {
-    // skip registers if they have the same key
+	for (const [key, value] of Object.entries(CpuRegisterGroup.register16Bit)) {
+		// skip registers if they have the same key
 
-    test(`Tests register  ${key} to get the value 0xff00 `, () => {
-      LDR16N16(value, 0xff00);
-      expect(value.getRegister()).toBe(0xff00);
-    });
-  }
+		test(`Tests register  ${key} to get the value 0xff00 `, () => {
+			LDR16N16(value, 0xff00);
+			expect(value.getRegister()).toBe(0xff00);
+		});
+	}
+});
+
+describe('This will test LD[HL], R8', () => {
+	const CpuGroupRegister = new CPU_Registers_Group();
+	CpuGroupRegister.register16Bit.HL.setRegister(0xffff);
+	// todo: replace this once implemented a good memory
+	const dummyMemory = new Uint8Array(0x10000);
+	for (const [key, value] of Object.entries(CpuGroupRegister.register)) {
+		test(
+			'This will add the number 0x00FF to register when [HL] = 0xFFFF' + key,
+			() => {
+				value.setRegister(0xff);
+
+				LDHLR8(CpuGroupRegister.register16Bit.HL, value, dummyMemory);
+
+				expect(
+					dummyMemory[CpuGroupRegister.register16Bit.HL.getRegister()]
+				).toBe(0xff);
+			}
+		);
+	}
 });
