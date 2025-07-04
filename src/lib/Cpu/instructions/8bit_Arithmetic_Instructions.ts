eb93dccbd78d84f1f1e370b09822f9c68aaa6b38
Lue Kely <luekely43110@gmail.com>
2025-07-04 17:51:41 +0800

    added ADCAHL

--- src/lib/Cpu/instructions/8bit_Arithmetic_Instructions.ts
+++ src/lib/Cpu/instructions/8bit_Arithmetic_Instructions.ts
@@ -1,5 +1,6 @@
+import type { Ram } from "../../Ram/Ram";
 import type { Cpu_Flag_Register } from "../CPU_Flag_Register";
-import type { Cpu_Register } from "../CPU_Register";
+import type { Cpu_Register, Cpu_Register_16Bit } from "../CPU_Register";
 
 function ADCAR8(
   register8: Cpu_Register,
@@ -18,12 +19,8 @@ function ADCAR8(
     registerF.clearZFlag();
   }
 
-  // Raise N flag to zero.
-  if (sum) {
-    registerF.clearNFlag();
-  } else {
-    registerF.clearNFlag();
-  }
+  // Clear N flag to during this operation.
+  registerF.clearNFlag();
 
   // Raise Half Carry flag if overlow from bit 3.
   if (sum > 0xf) {
@@ -42,4 +39,45 @@ function ADCAR8(
   registerA.setRegister(sum);
 }
 
-export { ADCAR8 };
+function ADCAHL(
+  pointer: Cpu_Register_16Bit,
+  memory: Ram,
+  registerF: Cpu_Flag_Register,
+  registerA: Cpu_Register
+) {
+  const sum =
+    (memory.getMemoryAt(pointer.getRegister()) +
+      registerF.getCYFlag() +
+      registerA.getRegister()) &
+    0xff;
+
+  //  set the flag registers
+
+  // Raise z flag if result is zero.
+  if (sum == 0) {
+    registerF.setZFlag();
+  } else {
+    registerF.clearZFlag();
+  }
+
+  // Clear N flag to during this operation.
+  registerF.clearNFlag();
+
+  // Raise Half Carry flag if overlow from bit 3.
+  if (sum > 0xf) {
+    registerF.setHFlag();
+  } else {
+    registerF.clearHFlag();
+  }
+
+  // Raise Carry flag if overlow from bit 7.
+  if (sum > 0xff) {
+    registerF.setCYFlag();
+  } else {
+    registerF.clearCYFlag();
+  }
+
+  registerA.setRegister(sum);
+}
+
+export { ADCAR8, ADCAHL };
