925cf57d53f9a778d49d02b8912d255d9aa7434a
Lue Kely <luekely43110@gmail.com>
2025-07-05 14:44:31 +0800

    added all the ADD to register A functions

--- src/lib/Cpu/__tests__/8bit_Arithmetic_Instructions.test.ts
+++ src/lib/Cpu/__tests__/8bit_Arithmetic_Instructions.test.ts
@@ -4,6 +4,9 @@ import {
   ADCAHL,
   ADCAN8,
   ADCAR8,
+  ADDAHL,
+  ADDAN8,
+  ADDAR8,
 } from "../instructions/8bit_Arithmetic_Instructions";
 import { Ram } from "../../Ram/Ram";
 
@@ -141,6 +144,152 @@ describe("ADCAN8 Functionalitys", () => {
     const CPU = new CPU_Registers_Group();
     CPU.register.A.setRegister(0x0);
     ADCAN8(0, CPU.register.A, CPU.register.F);
+
+    expect(CPU.register.A.getRegister()).toBe(0);
+    expect(CPU.register.F.getFRegister()).toBe(0b1000_0000);
+    expect(CPU.register.F.getZFlag()).toBe(1);
+    expect(CPU.register.F.getNFlag()).toBe(0);
+    expect(CPU.register.F.getHFlag()).toBe(0);
+    expect(CPU.register.F.getCYFlag()).toBe(0);
+  });
+});
+
+// ADD functions
+
+describe("ADDAR8 Functionalitys", () => {
+  test("The result of adding the value of register B to A is 1 — flags should all be 0", () => {
+    const CPU = new CPU_Registers_Group();
+    CPU.register.A.setRegister(0x0);
+    CPU.register.B.setRegister(0x1);
+    ADDAR8(CPU.register.B, CPU.register.F, CPU.register.A);
+
+    expect(CPU.register.A.getRegister()).toBe(0x1);
+
+    expect(CPU.register.F.getZFlag()).toBe(0);
+    expect(CPU.register.F.getNFlag()).toBe(0);
+    expect(CPU.register.F.getCYFlag()).toBe(0);
+    expect(CPU.register.F.getHFlag()).toBe(0);
+  });
+
+  test("The result of adding the value of register B to A is 15 — flags of the Halfcarry should raise ", () => {
+    const CPU = new CPU_Registers_Group();
+    CPU.register.A.setRegister(0x0e);
+    CPU.register.B.setRegister(2);
+    CPU.register.F.setCYFlag();
+
+    ADDAR8(CPU.register.B, CPU.register.F, CPU.register.A);
+
+    expect(CPU.register.A.getRegister()).toBe(16);
+    expect(CPU.register.F.getFRegister()).toBeGreaterThan(0);
+    expect(CPU.register.F.getZFlag()).toBe(0);
+    expect(CPU.register.F.getNFlag()).toBe(0);
+    expect(CPU.register.F.getHFlag()).toBe(1);
+    expect(CPU.register.F.getCYFlag()).toBe(0);
+  });
+
+  test("The result of adding the value of register B to A is 0 — flags of the Zero should raise", () => {
+    const CPU = new CPU_Registers_Group();
+    CPU.register.A.setRegister(0);
+    CPU.register.B.setRegister(0);
+
+    ADDAR8(CPU.register.B, CPU.register.F, CPU.register.A);
+
+    expect(CPU.register.A.getRegister()).toBe(0);
+    expect(CPU.register.F.getFRegister()).toBeGreaterThan(0);
+    expect(CPU.register.F.getZFlag()).toBe(1);
+    expect(CPU.register.F.getNFlag()).toBe(0);
+    expect(CPU.register.F.getHFlag()).toBe(0);
+    expect(CPU.register.F.getCYFlag()).toBe(0);
+  });
+});
+
+describe("ADDAHL Functionalitys", () => {
+  test("The result of adding the value of [HL] to A is 1 — flags should all be 0", () => {
+    const CPU = new CPU_Registers_Group();
+    const dummyMemory = new Ram();
+    CPU.register16Bit.HL.setRegister(0xff);
+    dummyMemory.setMemoryAt(CPU.register16Bit.HL.getRegister(), 0x01);
+    CPU.register.A.setRegister(0x0);
+
+    ADDAHL(CPU.register16Bit.HL, dummyMemory, CPU.register.F, CPU.register.A);
+
+    expect(CPU.register.A.getRegister()).toBe(1);
+    expect(CPU.register.F.getFRegister()).toBe(0);
+    expect(CPU.register.F.getZFlag()).toBe(0);
+    expect(CPU.register.F.getNFlag()).toBe(0);
+    expect(CPU.register.F.getHFlag()).toBe(0);
+    expect(CPU.register.F.getCYFlag()).toBe(0);
+  });
+
+  test("The result of adding the value of [HL] to A is 16 — flags of the Halfcarry should raise ", () => {
+    const CPU = new CPU_Registers_Group();
+    const dummyMemory = new Ram();
+    CPU.register16Bit.HL.setRegister(0xff);
+    dummyMemory.setMemoryAt(CPU.register16Bit.HL.getRegister(), 2);
+    CPU.register.A.setRegister(0x0e);
+    CPU.register.F.setCYFlag();
+
+    ADDAHL(CPU.register16Bit.HL, dummyMemory, CPU.register.F, CPU.register.A);
+
+    expect(CPU.register.A.getRegister()).toBe(16);
+    expect(CPU.register.F.getFRegister()).toBeGreaterThan(0);
+    expect(CPU.register.F.getZFlag()).toBe(0);
+    expect(CPU.register.F.getNFlag()).toBe(0);
+    expect(CPU.register.F.getHFlag()).toBe(1);
+    expect(CPU.register.F.getCYFlag()).toBe(0);
+  });
+
+  test("The result of adding the value of [HL] to A is 0 — flags of the Zero should raise", () => {
+    const CPU = new CPU_Registers_Group();
+    const dummyMemory = new Ram();
+    CPU.register16Bit.HL.setRegister(0xff);
+    dummyMemory.setMemoryAt(CPU.register16Bit.HL.getRegister(), 0x00);
+    CPU.register.A.setRegister(0x0);
+
+    ADDAHL(CPU.register16Bit.HL, dummyMemory, CPU.register.F, CPU.register.A);
+
+    expect(CPU.register.A.getRegister()).toBe(0);
+    expect(CPU.register.F.getFRegister()).toBe(0b1000_0000);
+    expect(CPU.register.F.getZFlag()).toBe(1);
+    expect(CPU.register.F.getNFlag()).toBe(0);
+    expect(CPU.register.F.getHFlag()).toBe(0);
+    expect(CPU.register.F.getCYFlag()).toBe(0);
+  });
+});
+
+describe("ADDAN8 Functionalitys", () => {
+  test("The result of adding the value of N8(0) to A is 1 — flags should all be 0", () => {
+    const CPU = new CPU_Registers_Group();
+    CPU.register.A.setRegister(0x0);
+    ADDAN8(1, CPU.register.A, CPU.register.F);
+
+    expect(CPU.register.A.getRegister()).toBe(1);
+    expect(CPU.register.F.getFRegister()).toBe(0);
+    expect(CPU.register.F.getZFlag()).toBe(0);
+    expect(CPU.register.F.getNFlag()).toBe(0);
+    expect(CPU.register.F.getHFlag()).toBe(0);
+    expect(CPU.register.F.getCYFlag()).toBe(0);
+  });
+
+  test("The result of adding the value of [HL] to A is 16 — flags of the Halfcarry should raise ", () => {
+    const CPU = new CPU_Registers_Group();
+    CPU.register.A.setRegister(0x0e);
+    CPU.register.F.setCYFlag();
+
+    ADDAN8(2, CPU.register.A, CPU.register.F);
+
+    expect(CPU.register.A.getRegister()).toBe(16);
+    expect(CPU.register.F.getFRegister()).toBeGreaterThan(0);
+    expect(CPU.register.F.getZFlag()).toBe(0);
+    expect(CPU.register.F.getNFlag()).toBe(0);
+    expect(CPU.register.F.getHFlag()).toBe(1);
+    expect(CPU.register.F.getCYFlag()).toBe(0);
+  });
+
+  test("The result of adding the value of [HL] to A is 0 — flags of the Zero should raise", () => {
+    const CPU = new CPU_Registers_Group();
+    CPU.register.A.setRegister(0x0);
+    ADDAN8(0, CPU.register.A, CPU.register.F);
 
     expect(CPU.register.A.getRegister()).toBe(0);
     expect(CPU.register.F.getFRegister()).toBe(0b1000_0000);
--- src/lib/Cpu/instructions/8bit_Arithmetic_Instructions.ts
+++ src/lib/Cpu/instructions/8bit_Arithmetic_Instructions.ts
@@ -3,6 +3,7 @@ import { validateR8Arythmatic } from "../../utils/instructions/instruction_utils
 import type { Cpu_Flag_Register } from "../CPU_Flag_Register";
 import type { Cpu_Register, Cpu_Register_16Bit } from "../CPU_Register";
 
+// add the carry flag functions
 function ADCAR8(
   register8: Cpu_Register,
   registerF: Cpu_Flag_Register,
@@ -44,4 +45,43 @@ function ADCAN8(
   registerA.setRegister(sum);
 }
 
-export { ADCAR8, ADCAHL, ADCAN8 };
+// no carry flag functions
+function ADDAR8(
+  register8: Cpu_Register,
+  registerF: Cpu_Flag_Register,
+  registerA: Cpu_Register
+) {
+  const sum = (register8.getRegister() + registerA.getRegister()) & 0xff;
+
+  //  validate sum with the flag registers
+  validateR8Arythmatic(sum, registerF);
+
+  registerA.setRegister(sum);
+}
+
+function ADDAHL(
+  pointer: Cpu_Register_16Bit,
+  memory: Ram,
+  registerF: Cpu_Flag_Register,
+  registerA: Cpu_Register
+) {
+  const sum =
+    (memory.getMemoryAt(pointer.getRegister()) + registerA.getRegister()) &
+    0xff;
+
+  //  validate sum with the flag registers
+  validateR8Arythmatic(sum, registerF);
+  registerA.setRegister(sum);
+}
+
+function ADDAN8(
+  value: number,
+  registerA: Cpu_Register,
+  registerF: Cpu_Flag_Register
+) {
+  const sum = (value + registerA.getRegister()) & 0xff;
+  validateR8Arythmatic(sum, registerF);
+  registerA.setRegister(sum);
+}
+
+export { ADCAR8, ADCAHL, ADCAN8, ADDAHL, ADDAN8, ADDAR8 };
