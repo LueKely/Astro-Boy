8e7c9dd34bd9c3af213f4a00d147739b0e836e78
Lue Kely <luekely43110@gmail.com>
2025-07-13 10:24:01 +0800

    added tests for ORAHL, ORAN8, ORAR8

--- src/lib/Cpu/__tests__/Bitwise_Instructions.test.ts
+++ src/lib/Cpu/__tests__/Bitwise_Instructions.test.ts
@@ -5,6 +5,9 @@ import {
   ANDAN8,
   ANDAR8,
   CPL,
+  ORAHL,
+  ORAN8,
+  ORAR8,
 } from "../instructions/Bitwise_Logic_Instructions";
 import { Ram } from "../../Ram/Ram";
 
@@ -60,7 +63,7 @@ describe("Functionalities of AND A, R8", () => {
   });
 });
 
-describe("Functionalities of AND A, R8", () => {
+describe("Functionalities of AND A, [HL]", () => {
   test("the value should be zero and the flag value should correspond", () => {
     const CPU = new CPU_Registers_Group();
     const { A, F } = CPU.register;
@@ -157,7 +160,7 @@ describe("Functionalities of AND A, N8", () => {
   });
 });
 
-describe("Tests the function of CPL", () => {
+describe("Tests the functions of CPL", () => {
   test("The value 1111_0000 should be 0000_1111; N and H flags should raise", () => {
     const CPU = new CPU_Registers_Group();
     const { A, F } = CPU.register;
@@ -166,5 +169,104 @@ describe("Tests the function of CPL", () => {
     CPL(A, F);
     expect(A.getRegister()).toBe(0b0000_1111);
     expect(F.getRegister()).toBe(0b0110_0000);
+  });
+});
+
+describe("Tests the fucntionality of OR A,R8", () => {
+  test("the value should be zero and the flag value should correspond", () => {
+    const CPU = new CPU_Registers_Group();
+    const { A, C, F } = CPU.register;
+
+    A.setRegister(0b0000_0000);
+    C.setRegister(0b0000_0000);
+
+    ORAR8(A, C, F);
+
+    expect(A.getRegister()).toBe(0);
+    expect(F.getRegister()).toBe(0b1000_0000);
+  });
+
+  test("the value should be 144 and the flag value should correspond", () => {
+    const CPU = new CPU_Registers_Group();
+    const { A, C, F } = CPU.register;
+
+    A.setRegister(0b1000_0000);
+    C.setRegister(0b0001_0000);
+
+    ORAR8(A, C, F);
+
+    expect(A.getRegister()).toBe(0b1001_0000);
+    expect(F.getRegister()).toBe(0b0000_0000);
+  });
+});
+
+describe("Functionalities of OR A, [HL]", () => {
+  test("the value should be zero and the flag value should correspond", () => {
+    const CPU = new CPU_Registers_Group();
+    const { A, F } = CPU.register;
+    const { HL } = CPU.register16Bit;
+    const dummyRam = new Ram();
+
+    A.setRegister(0b0000_0000);
+    HL.setRegister(0b0000_0000);
+    dummyRam.setMemoryAt(HL.getRegister(), 0b0000_0000);
+
+    ORAHL(A, HL, F, dummyRam);
+
+    expect(A.getRegister()).toBe(0);
+    expect(F.getRegister()).toBe(0b1000_0000);
+  });
+  test("the value should be 256 and the flag value should correspond", () => {
+    const CPU = new CPU_Registers_Group();
+    const { A, C, F } = CPU.register;
+    const { HL } = CPU.register16Bit;
+    const dummyRam = new Ram();
+
+    A.setRegister(0b1010_1111);
+    HL.setRegister(0b1111_0000);
+    dummyRam.setMemoryAt(HL.getRegister(), 0b1111_0000);
+
+    ORAHL(A, HL, F, dummyRam);
+
+    expect(A.getRegister()).toBe(0b1111_1111);
+    expect(F.getRegister()).toBe(0b0000_0000);
+  });
+});
+
+describe("Tests the fucntionality of OR A,R8", () => {
+  test("the value should be zero and the flag value should correspond", () => {
+    const CPU = new CPU_Registers_Group();
+    const { A, F } = CPU.register;
+
+    A.setRegister(0b0000_0000);
+
+    ORAN8(A, 0b0000_0000, F);
+
+    expect(A.getRegister()).toBe(0);
+    expect(F.getRegister()).toBe(0b1000_0000);
+  });
+
+  test("the value should be zero and the flag value should correspond", () => {
+    const CPU = new CPU_Registers_Group();
+    const { A, F } = CPU.register;
+
+    A.setRegister(0b1111_1111);
+
+    ORAN8(A, 0b1111_1111, F);
+
+    expect(A.getRegister()).toBe(0b1111_1111);
+    expect(F.getRegister()).toBe(0b0000_0000);
+  });
+
+  test("the value should be 144 and the flag value should correspond", () => {
+    const CPU = new CPU_Registers_Group();
+    const { A, F } = CPU.register;
+
+    A.setRegister(0b1000_0000);
+
+    ORAN8(A, 0b0001_0000, F);
+
+    expect(A.getRegister()).toBe(0b1001_0000);
+    expect(F.getRegister()).toBe(0b0000_0000);
   });
 });
