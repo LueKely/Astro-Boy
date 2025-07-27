5dca7d65dba47ede386c714587eb3f28529a7efe
Lue Kely <luekely43110@gmail.com>
2025-07-27 20:29:18 +0800

    i will finish this tomorrow frfr

--- src/lib/Cpu/CPU.ts
+++ src/lib/Cpu/CPU.ts
@@ -1,32 +1,10 @@
-import { Ram } from "../Ram/Ram";
-import { CpuOpcodeRecord } from "./CPU_Opcode_Record";
-import { Cpu_Register_File } from "./CPU_Register_File";
+import { Ram } from '../Ram/Ram';
+import { Cpu_Register_File } from './CPU_Register_File';
+import { Cpu_Scheduler } from './CPU_Scheduler';
 
 export class CPU {
-  readonly registers = new Cpu_Register_File();
-  readonly ram = new Ram();
-
-  private lowByte = 0;
-  private highByte = 0;
-  private opCodes = CpuOpcodeRecord(this);
-
-  readByte(pointer: number, cartridge: []) {
-    return cartridge[pointer];
-  }
-
-  setLowByte(value: number) {
-    this.lowByte = value & 0b0000_0011;
-  }
-
-  setHighByte(value: number) {
-    this.highByte = (value & 0b0000_1100) >> 2;
-  }
-
-  getLowByte() {
-    return this.lowByte;
-  }
-
-  getHighByte() {
-    return this.highByte;
-  }
+	readonly registers = new Cpu_Register_File();
+	readonly ram = new Ram();
+	readonly scheduler = new Cpu_Scheduler(this);
+	// readonly catridge;
 }
--- src/lib/Cpu/CPU_Scheduler.ts
+++ src/lib/Cpu/CPU_Scheduler.ts
@@ -1,3 +1,45 @@
+import type { CPU } from './CPU';
+import { CpuOpcodeRecord } from './CPU_Opcode_Record';
+import type { IOpCodeEntry } from './types/Opcode';
+
+// dear viewer
+// i lue, made an attempt to make a working scheduler
+// for the machine cycle of this bad boy
+// tho due to nature of how my opcodes work
+// some of the machine cycles will be just logs i guess
+// hopefully it'll all be bebetter soon lol
+
 export class Cpu_Scheduler {
-  private queue = [];
+	private queue = [];
+	private lowByte = 0;
+	private highByte = 0;
+	private cpu: CPU;
+	private opCodes: Record<number, IOpCodeEntry>;
+
+	constructor(CPU: CPU) {
+		this.cpu = CPU;
+		this.opCodes = CpuOpcodeRecord(this.cpu);
+	}
+
+	private readByte(pointer: number, cartridge: []) {
+		return cartridge[pointer];
+	}
+
+	setLowByte(value: number) {
+		this.lowByte = value & 0b0000_0011;
+	}
+
+	setHighByte(value: number) {
+		this.highByte = (value & 0b0000_1100) >> 2;
+	}
+
+	getLowByte() {
+		return this.lowByte;
+	}
+
+	getHighByte() {
+		return this.highByte;
+	}
+
+	tick() {}
 }
