a7da962dd1e19c722effd5696dfcb62ea8088e34
Lue Kely <luekely43110@gmail.com>
2025-07-30 10:56:36 +0800

    added ADC and ADD opcodes

--- src/lib/Cpu/CPU_Opcode_Record.ts
+++ src/lib/Cpu/CPU_Opcode_Record.ts
@@ -1,5 +1,9 @@
 import type { Gameboy } from "../Gameboy";
 import {
+  ADCAHL,
+  ADCAR8,
+  ADDAHL,
+  ADDAR8,
   DECHL,
   DECR8,
   INCHL,
@@ -45,7 +49,7 @@ export function CpuOpcodeRecord(): Record<number, IOpCodeEntry> {
       ],
     },
 
-    0x24: {
+    0x14: {
       name: "INC D",
       cycles: 1,
       length: 1,
@@ -57,7 +61,7 @@ export function CpuOpcodeRecord(): Record<number, IOpCodeEntry> {
         },
       ],
     },
-    0x25: {
+    0x15: {
       name: "DEC D",
       cycles: 1,
       length: 1,
@@ -70,7 +74,7 @@ export function CpuOpcodeRecord(): Record<number, IOpCodeEntry> {
       ],
     },
 
-    0x14: {
+    0x0c: {
       name: "INC C",
       cycles: 1,
       length: 1,
@@ -81,7 +85,7 @@ export function CpuOpcodeRecord(): Record<number, IOpCodeEntry> {
         },
       ],
     },
-    0x15: {
+    0x0d: {
       name: "DEC C",
       cycles: 1,
       length: 1,
@@ -92,7 +96,7 @@ export function CpuOpcodeRecord(): Record<number, IOpCodeEntry> {
         },
       ],
     },
-    0x34: {
+    0x1c: {
       name: "INC E",
       cycles: 1,
       length: 1,
@@ -103,7 +107,7 @@ export function CpuOpcodeRecord(): Record<number, IOpCodeEntry> {
         },
       ],
     },
-    0x35: {
+    0x1d: {
       name: "DEC E",
       cycles: 1,
       length: 1,
@@ -114,7 +118,7 @@ export function CpuOpcodeRecord(): Record<number, IOpCodeEntry> {
         },
       ],
     },
-    0x44: {
+    0x24: {
       name: "INC H",
       cycles: 1,
       length: 1,
@@ -125,7 +129,7 @@ export function CpuOpcodeRecord(): Record<number, IOpCodeEntry> {
         },
       ],
     },
-    0x45: {
+    0x25: {
       name: "DEC H",
       cycles: 1,
       length: 1,
@@ -136,7 +140,7 @@ export function CpuOpcodeRecord(): Record<number, IOpCodeEntry> {
         },
       ],
     },
-    0x54: {
+    0x2c: {
       name: "INC L",
       cycles: 1,
       length: 1,
@@ -147,7 +151,7 @@ export function CpuOpcodeRecord(): Record<number, IOpCodeEntry> {
         },
       ],
     },
-    0x55: {
+    0x2d: {
       name: "DEC L",
       cycles: 1,
       length: 1,
@@ -158,7 +162,7 @@ export function CpuOpcodeRecord(): Record<number, IOpCodeEntry> {
         },
       ],
     },
-    0x64: {
+    0x34: {
       name: "INC [HL]",
       cycles: 3,
       length: 1,
@@ -182,7 +186,7 @@ export function CpuOpcodeRecord(): Record<number, IOpCodeEntry> {
         },
       ],
     },
-    0x65: {
+    0x35: {
       name: "DEC [HL]",
       cycles: 3,
       length: 1,
@@ -205,7 +209,7 @@ export function CpuOpcodeRecord(): Record<number, IOpCodeEntry> {
         },
       ],
     },
-    0x74: {
+    0x3c: {
       name: "INC A",
       cycles: 1,
       length: 1,
@@ -216,7 +220,7 @@ export function CpuOpcodeRecord(): Record<number, IOpCodeEntry> {
         },
       ],
     },
-    0x75: {
+    0x3d: {
       name: "DEC A",
       cycles: 1,
       length: 1,
@@ -228,13 +232,268 @@ export function CpuOpcodeRecord(): Record<number, IOpCodeEntry> {
       ],
     },
 
-    0x57: {
+    0x2f: {
       name: "CPL",
       cycles: 1,
       length: 1,
       jobs: [
         (dmg: Gameboy) => {
           CPL(dmg.registers.register.A, dmg.registers.register.F);
+          dmg.registers.pointers.PC.increment();
+        },
+      ],
+    },
+
+    0x80: {
+      name: "ADD B",
+      cycles: 1,
+      length: 1,
+      jobs: [
+        (dmg: Gameboy) => {
+          ADDAR8(
+            dmg.registers.register.B,
+            dmg.registers.register.F,
+            dmg.registers.register.A
+          );
+          dmg.registers.pointers.PC.increment();
+        },
+      ],
+    },
+    0x81: {
+      name: "ADD C",
+      cycles: 1,
+      length: 1,
+      jobs: [
+        (dmg: Gameboy) => {
+          ADDAR8(
+            dmg.registers.register.C,
+            dmg.registers.register.F,
+            dmg.registers.register.A
+          );
+          dmg.registers.pointers.PC.increment();
+        },
+      ],
+    },
+    0x82: {
+      name: "ADD D",
+      cycles: 1,
+      length: 1,
+      jobs: [
+        (dmg: Gameboy) => {
+          ADDAR8(
+            dmg.registers.register.D,
+            dmg.registers.register.F,
+            dmg.registers.register.A
+          );
+          dmg.registers.pointers.PC.increment();
+        },
+      ],
+    },
+    0x83: {
+      name: "ADD E",
+      cycles: 1,
+      length: 1,
+      jobs: [
+        (dmg: Gameboy) => {
+          ADDAR8(
+            dmg.registers.register.E,
+            dmg.registers.register.F,
+            dmg.registers.register.A
+          );
+          dmg.registers.pointers.PC.increment();
+        },
+      ],
+    },
+    0x84: {
+      name: "ADD H",
+      cycles: 1,
+      length: 1,
+      jobs: [
+        (dmg: Gameboy) => {
+          ADDAR8(
+            dmg.registers.register.H,
+            dmg.registers.register.F,
+            dmg.registers.register.A
+          );
+          dmg.registers.pointers.PC.increment();
+        },
+      ],
+    },
+    0x85: {
+      name: "ADD L",
+      cycles: 1,
+      length: 1,
+      jobs: [
+        (dmg: Gameboy) => {
+          ADDAR8(
+            dmg.registers.register.D,
+            dmg.registers.register.F,
+            dmg.registers.register.A
+          );
+          dmg.registers.pointers.PC.increment();
+        },
+      ],
+    },
+    0x86: {
+      name: "ADD [HL]",
+      cycles: 2,
+      length: 1,
+      jobs: [
+        (dmg: Gameboy) => {
+          console.log(
+            "MemAdd Value:",
+            dmg.ram.getMemoryAt(dmg.registers.register16Bit.HL.getRegister())
+          );
+        },
+        (dmg: Gameboy) => {
+          ADDAHL(
+            dmg.registers.register16Bit.HL,
+            dmg.ram,
+            dmg.registers.register.F,
+            dmg.registers.register.A
+          );
+          dmg.registers.pointers.PC.increment();
+        },
+      ],
+    },
+    0x87: {
+      name: "ADD A",
+      cycles: 1,
+      length: 1,
+      jobs: [
+        (dmg: Gameboy) => {
+          ADDAR8(
+            dmg.registers.register.A,
+            dmg.registers.register.F,
+            dmg.registers.register.A
+          );
+          dmg.registers.pointers.PC.increment();
+        },
+      ],
+    },
+    0x88: {
+      name: "ADC B",
+      cycles: 1,
+      length: 1,
+      jobs: [
+        (dmg: Gameboy) => {
+          ADCAR8(
+            dmg.registers.register.B,
+            dmg.registers.register.F,
+            dmg.registers.register.A
+          );
+          dmg.registers.pointers.PC.increment();
+        },
+      ],
+    },
+    0x89: {
+      name: "ADC C",
+      cycles: 1,
+      length: 1,
+      jobs: [
+        (dmg: Gameboy) => {
+          ADCAR8(
+            dmg.registers.register.C,
+            dmg.registers.register.F,
+            dmg.registers.register.A
+          );
+          dmg.registers.pointers.PC.increment();
+        },
+      ],
+    },
+    0x8a: {
+      name: "ADC D",
+      cycles: 1,
+      length: 1,
+      jobs: [
+        (dmg: Gameboy) => {
+          ADCAR8(
+            dmg.registers.register.D,
+            dmg.registers.register.F,
+            dmg.registers.register.A
+          );
+          dmg.registers.pointers.PC.increment();
+        },
+      ],
+    },
+    0x8b: {
+      name: "ADC E",
+      cycles: 1,
+      length: 1,
+      jobs: [
+        (dmg: Gameboy) => {
+          ADCAR8(
+            dmg.registers.register.E,
+            dmg.registers.register.F,
+            dmg.registers.register.A
+          );
+          dmg.registers.pointers.PC.increment();
+        },
+      ],
+    },
+    0x8c: {
+      name: "ADC D",
+      cycles: 1,
+      length: 1,
+      jobs: [
+        (dmg: Gameboy) => {
+          ADCAR8(
+            dmg.registers.register.H,
+            dmg.registers.register.F,
+            dmg.registers.register.A
+          );
+          dmg.registers.pointers.PC.increment();
+        },
+      ],
+    },
+    0x8d: {
+      name: "ADC D",
+      cycles: 1,
+      length: 1,
+      jobs: [
+        (dmg: Gameboy) => {
+          ADCAR8(
+            dmg.registers.register.L,
+            dmg.registers.register.F,
+            dmg.registers.register.A
+          );
+          dmg.registers.pointers.PC.increment();
+        },
+      ],
+    },
+    0x8e: {
+      name: "ADC [HL]",
+      cycles: 2,
+      length: 1,
+      jobs: [
+        (dmg: Gameboy) => {
+          console.log(
+            "MemAdd Value:",
+            dmg.ram.getMemoryAt(dmg.registers.register16Bit.HL.getRegister())
+          );
+        },
+        (dmg: Gameboy) => {
+          ADCAHL(
+            dmg.registers.register16Bit.HL,
+            dmg.ram,
+            dmg.registers.register.F,
+            dmg.registers.register.A
+          );
+          dmg.registers.pointers.PC.increment();
+        },
+      ],
+    },
+    0x8f: {
+      name: "ADC A",
+      cycles: 1,
+      length: 1,
+      jobs: [
+        (dmg: Gameboy) => {
+          ADCAR8(
+            dmg.registers.register.A,
+            dmg.registers.register.F,
+            dmg.registers.register.A
+          );
           dmg.registers.pointers.PC.increment();
         },
       ],
