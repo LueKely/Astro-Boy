66e445171dafd4490995d320047f837429e4bd58
Lue Kely <luekely43110@gmail.com>
2025-08-02 12:24:31 +0800

    todo jump instructions

--- src/lib/Cpu/instructions/Jumps_And _Subroutine_Instructions.ts
+++ src/lib/Cpu/instructions/Jumps_And _Subroutine_Instructions.ts
@@ -1,4 +1,4 @@
 import type { Ram } from "../../Ram/Ram";
 import type { Program_Counter_Register } from "../CPU_Pointer_Register";
-
+// TODO THIS
 function CALLN16(PC: Program_Counter_Register, Ram: Ram, OPCodes: void) {}
