c169da18f0bde74d8cf5f67cead01df744af54ab
Lue Kely <luekely43110@gmail.com>
2025-08-06 10:22:35 +0800

    tests for scf

--- src/lib/Cpu/__tests__/Carry_Flag_Instructions.test.ts
+++ src/lib/Cpu/__tests__/Carry_Flag_Instructions.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test } from "vitest";
 import { Cpu_Register_File } from "../CPU_Register_File";
-import { CCF } from "../instructions/Carry_Flag_Instructions";
+import { CCF, SCF } from "../instructions/Carry_Flag_Instructions";
 
 describe("CCF", () => {
   test("C should be 0 and H, N should be 0", () => {
@@ -17,6 +17,17 @@ describe("CCF", () => {
     const group = new Cpu_Register_File();
     const { F } = group.register;
     CCF(F);
+    expect(F.getCYFlag()).toBe(1);
+    expect(F.getHFlag()).toBe(0);
+    expect(F.getNFlag()).toBe(0);
+  });
+});
+
+describe("SCF", () => {
+  test("C should be 1 and H, N should be 0", () => {
+    const group = new Cpu_Register_File();
+    const { F } = group.register;
+    SCF(F);
     expect(F.getCYFlag()).toBe(1);
     expect(F.getHFlag()).toBe(0);
     expect(F.getNFlag()).toBe(0);
