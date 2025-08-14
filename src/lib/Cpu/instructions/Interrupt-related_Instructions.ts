14671a198c3fbfa6626ae40f66dbf38114b7e57f
Lue Kely <luekely43110@gmail.com>
2025-08-14 09:08:47 +0800

    adjusments

--- src/lib/Cpu/instructions/Interrupt-related_Instructions.ts
+++ src/lib/Cpu/instructions/Interrupt-related_Instructions.ts
@@ -1,41 +1,41 @@
-import type { Gameboy } from '../../Gameboy';
+import type { Gameboy } from "../../Gameboy";
 
 function EI() {
-	return [
-		(dmg: Gameboy) => {
-			dmg.registers.IME.raiseFlag();
-			dmg.registers.pointers.SP.decrement();
-		},
-	];
+  return [
+    (dmg: Gameboy) => {
+      dmg.registers.IME.raiseFlag();
+      dmg.registers.pointers.SP.decrement();
+    },
+  ];
 }
 
 function DI() {
-	return [
-		(dmg: Gameboy) => {
-			dmg.registers.IME.clearFlag();
-			dmg.registers.pointers.SP.decrement();
-		},
-	];
+  return [
+    (dmg: Gameboy) => {
+      dmg.registers.IME.clearFlag();
+      dmg.registers.pointers.SP.decrement();
+    },
+  ];
 }
 
 function HALT() {
-	return [
-		(dmg: Gameboy) => {
-			if (!dmg.registers.HALT) {
-				dmg.registers.HALT = true;
-			}
-		},
-	];
+  return [
+    (dmg: Gameboy) => {
+      if (!dmg.registers.HALT) {
+        dmg.registers.HALT = true;
+      }
+    },
+  ];
 }
 
 function STOP() {
-	return [
-		(dmg: Gameboy) => {
-			if (!dmg.registers.HALT) {
-				dmg.registers.STOP = true;
-			}
-		},
-	];
+  return [
+    (dmg: Gameboy) => {
+      if (!dmg.registers.HALT) {
+        dmg.registers.STOP = true;
+      }
+    },
+  ];
 }
 
 // insert halt here
