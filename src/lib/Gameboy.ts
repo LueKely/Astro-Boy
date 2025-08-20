0bb767a8ad824f1bdf083f92994f55d704b878b8
Lue Kely <luekely43110@gmail.com>
2025-08-20 10:36:10 +0800

    fixed 2 bugs

--- src/lib/Gameboy.ts
+++ src/lib/Gameboy.ts
@@ -4,60 +4,57 @@ import { Cpu_Register_File } from './Cpu/CPU_Register_File';
 import { Cpu_Scheduler } from './Cpu/CPU_Scheduler';
 
 export class Gameboy {
-	readonly registers: Cpu_Register_File;
-	readonly ram: Ram;
-	readonly cartridge: GameBoyCatridge;
-	readonly scheduler: Cpu_Scheduler;
+  readonly registers: Cpu_Register_File;
+  readonly ram: Ram;
+  readonly cartridge: GameBoyCatridge;
+  readonly scheduler: Cpu_Scheduler;
 
-	constructor(game: ArrayBuffer) {
-		this.registers = new Cpu_Register_File();
-		this.ram = new Ram();
-		this.cartridge = new GameBoyCatridge(game);
-		console.log(this.cartridge.CartDataToBytes);
-		this.scheduler = new Cpu_Scheduler(this);
-	}
+  constructor(game: ArrayBuffer) {
+    this.registers = new Cpu_Register_File();
+    this.ram = new Ram();
+    this.cartridge = new GameBoyCatridge(game);
+    console.log(this.cartridge.CartDataToBytes);
+    this.scheduler = new Cpu_Scheduler(this);
+  }
 
-	log() {
-		const systemState = {
-			'Register A': this.registers.register.A.getRegister(),
-			'Register B': this.registers.register.B.getRegister(),
-			'Register C': this.registers.register.C.getRegister(),
-			'Register D': this.registers.register.D.getRegister(),
-			'Register E': this.registers.register.E.getRegister(),
-			'Register H': this.registers.register.H.getRegister(),
-			'Register L': this.registers.register.L.getRegister(),
-			'Register F': this.registers.register.F.getRegister().toString(2),
-			TempByte: this.registers.getTempByte(),
-			LowerByte: this.registers.getLowerByte(),
-			UpperByte: this.registers.getUpperByte(),
-			'Program Counter': this.registers.pointers.PC.getRegister(),
-			'Stack Pointer': this.registers.pointers.SP.getRegister(),
-			IME: this.registers.IME.getValue(),
-			IF: this.ram.getIF(),
-			IE: this.ram.getIE(),
-			HALT_BUG: this.registers.HALT_BUG,
-			HALT: this.registers.HALT,
-			STOP: this.registers.STOP,
-			'Memory 0xFF01': this.ram.getMemoryAt(0xff01),
-			'Memory 0xFF02': this.ram.getMemoryAt(0xff02),
-		};
-		console.table(systemState);
-	}
-	run() {
-		// requestAnimationFrame(() => {
-		// 	const result = this.ram.getMemoryAt(0xa001);
-
-		// 	if (result == 0x80) {
-		// 		console.log('TEST FINISHED!', result);
-
-		// 		return;
-		// 	}
-		// 	this.scheduler.tick();
-		// 	this.run();
-		// });
-
-		for (let index = 0; index < 1000; index++) {
-			this.scheduler.tick();
-		}
-	}
+  log() {
+    const systemState = {
+      'Register A': this.registers.register.A.getRegister(),
+      'Register B': this.registers.register.B.getRegister(),
+      'Register C': this.registers.register.C.getRegister(),
+      'Register D': this.registers.register.D.getRegister(),
+      'Register E': this.registers.register.E.getRegister(),
+      'Register H': this.registers.register.H.getRegister(),
+      'Register L': this.registers.register.L.getRegister(),
+      'Register F': this.registers.register.F.getRegister().toString(2),
+      TempByte: this.registers.getTempByte(),
+      LowerByte: this.registers.getLowerByte(),
+      UpperByte: this.registers.getUpperByte(),
+      'Program Counter': this.registers.pointers.PC.getRegister(),
+      'Stack Pointer': this.registers.pointers.SP.getRegister(),
+      IME: this.registers.IME.getValue(),
+      IF: this.ram.getIF(),
+      IE: this.ram.getIE(),
+      HALT_BUG: this.registers.HALT_BUG,
+      HALT: this.registers.HALT,
+      STOP: this.registers.STOP,
+      'Memory 0xFF01': this.ram.getMemoryAt(0xff01),
+      'Memory 0xFF02': this.ram.getMemoryAt(0xff02),
+    };
+    console.table(systemState);
+  }
+  run() {
+    // requestAnimationFrame(() => {
+    //   const result = this.ram.getMemoryAt(0xa001);
+    //   if (result == 0x80) {
+    //     console.log('TEST FINISHED!', result);
+    //     return;
+    //   }
+    //   this.scheduler.tick();
+    //   this.run();
+    // });
+    for (let index = 0; index < 3000; index++) {
+      this.scheduler.tick();
+    }
+  }
 }
--- src/lib/utils/instructions/instruction_utils.ts
+++ src/lib/utils/instructions/instruction_utils.ts
@@ -5,33 +5,33 @@ import type { Cpu_Flag_Register } from '../../Cpu/CPU_Flag_Register';
  * operations
  **/
 function validateR8Addition(
-	sum: number,
-	hflagSum: number,
-	flagRegister: Cpu_Flag_Register
+  sum: number,
+  hflagSum: number,
+  flagRegister: Cpu_Flag_Register
 ) {
-	// Raise z flag if result is zero.
-	if (sum == 0) {
-		flagRegister.setZFlag();
-	} else {
-		flagRegister.clearZFlag();
-	}
+  // Raise z flag if result is zero.
+  if (sum == 0) {
+    flagRegister.setZFlag();
+  } else {
+    flagRegister.clearZFlag();
+  }
 
-	// Clear N flag to during this operation.
-	flagRegister.clearNFlag();
+  // Clear N flag to during this operation.
+  flagRegister.clearNFlag();
 
-	// Raise Half Carry flag if overlow from bit 3.
-	if (hflagSum > 0xf) {
-		flagRegister.setHFlag();
-	} else {
-		flagRegister.clearHFlag();
-	}
+  // Raise Half Carry flag if overlow from bit 3.
+  if (hflagSum > 0xf) {
+    flagRegister.setHFlag();
+  } else {
+    flagRegister.clearHFlag();
+  }
 
-	// Raise Carry flag if overlow from bit 7.
-	if (sum > 0xff) {
-		flagRegister.setCYFlag();
-	} else {
-		flagRegister.clearCYFlag();
-	}
+  // Raise Carry flag if overlow from bit 7.
+  if (sum > 0xff) {
+    flagRegister.setCYFlag();
+  } else {
+    flagRegister.clearCYFlag();
+  }
 }
 
 /**
@@ -39,199 +39,199 @@ function validateR8Addition(
  * during R16 operations
  **/
 function validateR16Addition(sum: number, flagRegister: Cpu_Flag_Register) {
-	// Clear N flag to during this operation.
-	flagRegister.clearNFlag();
+  // Clear N flag to during this operation.
+  flagRegister.clearNFlag();
 
-	// Raise Half Carry flag if overlow from bit 11.
-	if (sum > 0xfff) {
-		flagRegister.setHFlag();
-	} else {
-		flagRegister.clearHFlag();
-	}
+  // Raise Half Carry flag if overlow from bit 11.
+  if (sum > 0xfff) {
+    flagRegister.setHFlag();
+  } else {
+    flagRegister.clearHFlag();
+  }
 
-	// Raise Carry flag if overlow from bit 15.
-	if (sum > 0xffff) {
-		flagRegister.setCYFlag();
-	} else {
-		flagRegister.clearCYFlag();
-	}
+  // Raise Carry flag if overlow from bit 15.
+  if (sum > 0xffff) {
+    flagRegister.setCYFlag();
+  } else {
+    flagRegister.clearCYFlag();
+  }
 }
 
 function validateR8Subtraction(
-	registerF: Cpu_Flag_Register,
-	minuend: number,
-	subtrahend: number,
-	carry: number = 0
+  registerF: Cpu_Flag_Register,
+  minuend: number,
+  subtrahend: number,
+  carry: number = 0
 ) {
-	const difference = minuend - subtrahend - carry;
+  const difference = minuend - subtrahend - carry;
 
-	// flag conditions below
-	if (difference == 0) {
-		registerF.setZFlag();
-	} else {
-		registerF.clearZFlag();
-	}
+  // flag conditions below
+  if (difference == 0) {
+    registerF.setZFlag();
+  } else {
+    registerF.clearZFlag();
+  }
 
-	registerF.setNFlag();
+  registerF.setNFlag();
 
-	if ((minuend & 0x0f) < (subtrahend & 0x0f) + carry) {
-		registerF.setHFlag();
-	} else {
-		registerF.clearHFlag();
-	}
-	if (subtrahend + carry > minuend) {
-		registerF.setCYFlag();
-	} else {
-		registerF.clearCYFlag();
-	}
+  if ((minuend & 0x0f) < (subtrahend & 0x0f) + carry) {
+    registerF.setHFlag();
+  } else {
+    registerF.clearHFlag();
+  }
+  if (subtrahend + carry > minuend) {
+    registerF.setCYFlag();
+  } else {
+    registerF.clearCYFlag();
+  }
 }
 
 function validateR8Decrement(registerF: Cpu_Flag_Register, value: number) {
-	const difference = value - 1;
-	// flag conditions below
-	registerF.setNFlag();
+  const difference = value - 1;
+  // flag conditions below
+  registerF.setNFlag();
 
-	if (difference == 0) {
-		registerF.setZFlag();
-	} else {
-		registerF.clearZFlag();
-	}
+  if (difference == 0) {
+    registerF.setZFlag();
+  } else {
+    registerF.clearZFlag();
+  }
 
-	if (0x1 > (value & 0x0f)) {
-		registerF.setHFlag();
-	} else {
-		registerF.clearHFlag();
-	}
+  if (0x1 > (value & 0x0f)) {
+    registerF.setHFlag();
+  } else {
+    registerF.clearHFlag();
+  }
 }
 
 function validateR8Increment(sum: number, flagRegister: Cpu_Flag_Register) {
-	// Raise z flag if result is zero.
-	if (sum == 0) {
-		flagRegister.setZFlag();
-	} else {
-		flagRegister.clearZFlag();
-	}
+  // Raise z flag if result is zero.
+  if ((sum & 0xff) == 0) {
+    flagRegister.setZFlag();
+  } else {
+    flagRegister.clearZFlag();
+  }
 
-	// Clear N flag to during this operation.
-	flagRegister.clearNFlag();
+  // Clear N flag to during this operation.
+  flagRegister.clearNFlag();
 
-	// Raise Half Carry flag if overlow from bit 3.
-	if (sum > 0xf) {
-		flagRegister.setHFlag();
-	} else {
-		flagRegister.clearHFlag();
-	}
+  // Raise Half Carry flag if overlow from bit 3.
+  if (sum > 0xf) {
+    flagRegister.setHFlag();
+  } else {
+    flagRegister.clearHFlag();
+  }
 }
 
 function validateAndOperation(result: number, flagRegister: Cpu_Flag_Register) {
-	if (result == 0) {
-		flagRegister.setZFlag();
-	} else {
-		flagRegister.clearZFlag();
-	}
-	flagRegister.clearNFlag();
-	flagRegister.setHFlag();
-	flagRegister.clearCYFlag();
+  if (result == 0) {
+    flagRegister.setZFlag();
+  } else {
+    flagRegister.clearZFlag();
+  }
+  flagRegister.clearNFlag();
+  flagRegister.setHFlag();
+  flagRegister.clearCYFlag();
 }
 
 // used both by XOR and OR operations
 function validateOrOperation(result: number, F: Cpu_Flag_Register) {
-	if (result == 0) {
-		F.setZFlag();
-	} else {
-		F.clearZFlag();
-	}
-	F.clearNFlag();
-	F.clearHFlag();
-	F.clearCYFlag();
+  if (result == 0) {
+    F.setZFlag();
+  } else {
+    F.clearZFlag();
+  }
+  F.clearNFlag();
+  F.clearHFlag();
+  F.clearCYFlag();
 }
 
 function validateBitFlagOperation(testBit: number, F: Cpu_Flag_Register) {
-	if (testBit == 0) {
-		F.setZFlag();
-	} else {
-		F.clearZFlag();
-	}
+  if (testBit == 0) {
+    F.setZFlag();
+  } else {
+    F.clearZFlag();
+  }
 
-	F.clearNFlag();
-	F.setHFlag();
+  F.clearNFlag();
+  F.setHFlag();
 }
 
 function validateBitShiftAccOperation(carryBit: number, F: Cpu_Flag_Register) {
-	if (carryBit == 1) {
-		F.setCYFlag();
-	} else {
-		F.clearCYFlag();
-	}
+  if (carryBit == 1) {
+    F.setCYFlag();
+  } else {
+    F.clearCYFlag();
+  }
 
-	F.clearHFlag();
-	F.clearNFlag();
-	F.clearZFlag();
+  F.clearHFlag();
+  F.clearNFlag();
+  F.clearZFlag();
 }
 
 function validateBitShiftOperation(
-	carryBit: number,
-	result: number,
-	F: Cpu_Flag_Register
+  carryBit: number,
+  result: number,
+  F: Cpu_Flag_Register
 ) {
-	if (carryBit == 1) {
-		F.setCYFlag();
-	} else {
-		F.clearCYFlag();
-	}
+  if (carryBit == 1) {
+    F.setCYFlag();
+  } else {
+    F.clearCYFlag();
+  }
 
-	if ((result & 0b1111_1111) == 0) {
-		F.setZFlag();
-	} else {
-		F.clearZFlag();
-	}
+  if ((result & 0b1111_1111) == 0) {
+    F.setZFlag();
+  } else {
+    F.clearZFlag();
+  }
 
-	F.clearHFlag();
-	F.clearNFlag();
+  F.clearHFlag();
+  F.clearNFlag();
 }
 
 function validateSwapOperation(result: number, F: Cpu_Flag_Register) {
-	if ((result & 0b1111_1111) == 0) {
-		F.setZFlag();
-	} else {
-		F.clearZFlag();
-	}
+  if ((result & 0b1111_1111) == 0) {
+    F.setZFlag();
+  } else {
+    F.clearZFlag();
+  }
 
-	F.clearCYFlag();
-	F.clearHFlag();
-	F.clearNFlag();
+  F.clearCYFlag();
+  F.clearHFlag();
+  F.clearNFlag();
 }
 
 // todo make it use for LD HL SP+e as well
 function validateADDSPe(lsbSP: number, e: number, F: Cpu_Flag_Register) {
-	F.clearZFlag();
-	F.clearNFlag();
+  F.clearZFlag();
+  F.clearNFlag();
 
-	const result = lsbSP + e;
+  const result = lsbSP + e;
 
-	if ((lsbSP & 0xf) + (e & 0xf) > 0xf) {
-		F.setHFlag();
-	} else {
-		F.clearHFlag();
-	}
-	if (result > 0xff) {
-		F.setCYFlag();
-	} else {
-		F.clearCYFlag();
-	}
+  if ((lsbSP & 0xf) + (e & 0xf) > 0xf) {
+    F.setHFlag();
+  } else {
+    F.clearHFlag();
+  }
+  if (result > 0xff) {
+    F.setCYFlag();
+  } else {
+    F.clearCYFlag();
+  }
 }
 
 export {
-	validateR8Addition,
-	validateR16Addition,
-	validateR8Subtraction,
-	validateR8Decrement,
-	validateR8Increment,
-	validateAndOperation,
-	validateOrOperation,
-	validateBitFlagOperation,
-	validateBitShiftAccOperation,
-	validateBitShiftOperation,
-	validateSwapOperation,
-	validateADDSPe,
+  validateR8Addition,
+  validateR16Addition,
+  validateR8Subtraction,
+  validateR8Decrement,
+  validateR8Increment,
+  validateAndOperation,
+  validateOrOperation,
+  validateBitFlagOperation,
+  validateBitShiftAccOperation,
+  validateBitShiftOperation,
+  validateSwapOperation,
+  validateADDSPe,
 };
