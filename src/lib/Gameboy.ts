59bdd49b1d821241aa97ccb5d465c38a427ac546
Lue Kely <luekely43110@gmail.com>
2025-08-21 13:14:52 +0800

    i dont like the debugger

--- src/lib/Gameboy.ts
+++ src/lib/Gameboy.ts
@@ -4,123 +4,74 @@ import { Cpu_Register_File } from './Cpu/CPU_Register_File';
 import { Cpu_Scheduler } from './Cpu/CPU_Scheduler';
 
 export class Gameboy {
-	readonly registers: Cpu_Register_File;
-	readonly ram: Ram;
-	readonly cartridge: GameBoyCatridge;
-	readonly scheduler: Cpu_Scheduler;
+  readonly registers: Cpu_Register_File;
+  readonly ram: Ram;
+  readonly cartridge: GameBoyCatridge;
+  readonly scheduler: Cpu_Scheduler;
 
-	// TODO create a class for timer
-	cycleBudget = 0;
-	lastTime = 0;
+  // TODO create a class for timer
+  cycleBudget = 0;
+  lastTime = 0;
 
-	constructor(game: ArrayBuffer) {
-		this.registers = new Cpu_Register_File();
-		this.ram = new Ram();
-		this.cartridge = new GameBoyCatridge(game);
-		console.log(this.cartridge.CartDataToBytes);
-		this.scheduler = new Cpu_Scheduler(this);
-		// timer
-		this.cycleBudget = 0;
-		this.lastTime = performance.now();
-	}
+  constructor(game: ArrayBuffer) {
+    this.registers = new Cpu_Register_File();
+    this.ram = new Ram();
+    this.cartridge = new GameBoyCatridge(game);
+    console.log(this.cartridge.CartDataToBytes);
+    this.scheduler = new Cpu_Scheduler(this);
+    // timer
+    this.cycleBudget = 0;
+    this.lastTime = performance.now();
+  }
 
-	log() {
-		const systemState = {
-			'Register A': this.registers.register.A.getRegister(),
-			'Register B': this.registers.register.B.getRegister(),
-			'Register C': this.registers.register.C.getRegister(),
-			'Register D': this.registers.register.D.getRegister(),
-			'Register E': this.registers.register.E.getRegister(),
-			'Register H': this.registers.register.H.getRegister(),
-			'Register L': this.registers.register.L.getRegister(),
-			'Register F': this.registers.register.F.getRegister().toString(2),
-			TempByte: this.registers.getTempByte(),
-			LowerByte: this.registers.getLowerByte(),
-			UpperByte: this.registers.getUpperByte(),
-			'Program Counter': this.registers.pointers.PC.getRegister(),
-			'Stack Pointer': this.registers.pointers.SP.getRegister(),
-			IME: this.registers.IME.getValue(),
-			IF: this.ram.getIF(),
-			IE: this.ram.getIE(),
-			HALT_BUG: this.registers.HALT_BUG,
-			HALT: this.registers.HALT,
-			STOP: this.registers.STOP,
-			'Memory 0xFF01': this.ram.getMemoryAt(0xff01),
-			'Memory 0xFF02': this.ram.getMemoryAt(0xff02),
-		};
-		console.table(systemState);
-	}
+  log() {
+    const systemState = {
+      'Register A': this.registers.register.A.getRegister(),
+      'Register B': this.registers.register.B.getRegister(),
+      'Register C': this.registers.register.C.getRegister(),
+      'Register D': this.registers.register.D.getRegister(),
+      'Register E': this.registers.register.E.getRegister(),
+      'Register H': this.registers.register.H.getRegister(),
+      'Register L': this.registers.register.L.getRegister(),
+      'Register F': this.registers.register.F.getRegister().toString(2),
+      TempByte: this.registers.getTempByte(),
+      LowerByte: this.registers.getLowerByte(),
+      UpperByte: this.registers.getUpperByte(),
+      'Program Counter': this.registers.pointers.PC.getRegister(),
+      'Stack Pointer': this.registers.pointers.SP.getRegister(),
+      IME: this.registers.IME.getValue(),
+      IF: this.ram.getIF(),
+      IE: this.ram.getIE(),
+      HALT_BUG: this.registers.HALT_BUG,
+      HALT: this.registers.HALT,
+      STOP: this.registers.STOP,
+      'Memory 0xFF01': this.ram.getMemoryAt(0xff01),
+      'Memory 0xFF02': this.ram.getMemoryAt(0xff02),
+    };
+    console.table(systemState);
+  }
 
-	run() {
-		// this.ratboDebugger();
+  run() {
+    // this.ratboDebugger();
 
-		// const now = performance.now();
-		// const elapsedMs = now - this.lastTime;
-		// this.lastTime = now;
+    // const now = performance.now();
+    // const elapsedMs = now - this.lastTime;
+    // this.lastTime = now;
 
-		// const M_CYCLES_PER_SEC = 4194304 / 4; // 1,048,576
-		// this.cycleBudget += elapsedMs * (M_CYCLES_PER_SEC / 1000);
+    // const M_CYCLES_PER_SEC = 4194304 / 4; // 1,048,576
+    // this.cycleBudget += elapsedMs * (M_CYCLES_PER_SEC / 1000);
 
-		// while (this.cycleBudget >= 1) {
-		// 	this.scheduler.tick();
-		// 	this.cycleBudget -= 1;
-		// }
+    // while (this.cycleBudget >= 1) {
+    // 	this.scheduler.tick();
+    // 	this.cycleBudget -= 1;
+    // }
 
-		// requestAnimationFrame(() => {
-		// 	this.run();
-		// });
+    // requestAnimationFrame(() => {
+    // 	this.run();
+    // });
 
-		for (let index = 0; index < 3000; index++) {
-			this.scheduler.tick();
-		}
-		this.ratboDebugger();
-	}
-	ratboDebugger() {
-		console.log('RATBO');
-
-		const A = document.querySelector('#A');
-		const B = document.querySelector('#B');
-		const C = document.querySelector('#C');
-		const D = document.querySelector('#D');
-		const E = document.querySelector('#E');
-		const F = document.querySelector('#F');
-		const H = document.querySelector('#H');
-		const L = document.querySelector('#L');
-		const SP = document.querySelector('#SP');
-		const PC = document.querySelector('#PC');
-		const Instruction = document.querySelector('#ins');
-
-		if (A)
-			A.textContent = this.registers.register.A.getRegister().toLocaleString();
-
-		if (B)
-			B.textContent = this.registers.register.A.getRegister().toLocaleString();
-		if (C)
-			C.textContent = this.registers.register.A.getRegister().toLocaleString();
-		if (D)
-			D.textContent = this.registers.register.A.getRegister().toLocaleString();
-		if (E)
-			E.textContent = this.registers.register.A.getRegister().toLocaleString();
-		if (F)
-			F.textContent = this.registers.register.A.getRegister().toLocaleString();
-		if (H)
-			H.textContent = this.registers.register.A.getRegister().toLocaleString();
-		if (L)
-			L.textContent = this.registers.register.A.getRegister().toLocaleString();
-		if (H)
-			H.textContent = this.registers.register.A.getRegister().toLocaleString();
-		if (PC)
-			PC.textContent =
-				this.registers.pointers.PC.getRegister().toLocaleString();
-		if (SP)
-			SP.textContent =
-				this.registers.pointers.PC.getRegister().toLocaleString();
-
-		if (Instruction) {
-			Instruction.textContent =
-				this.cartridge.CartDataToBytes[
-					this.registers.pointers.PC.getRegister()
-				].toLocaleString();
-		}
-	}
+    for (let index = 0; index < 3000; index++) {
+      this.scheduler.tick();
+    }
+  }
 }
