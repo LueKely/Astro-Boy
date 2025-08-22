3acf31d21a3f67f8d14562cea28493ae2ebac90b
Lue Kely <luekely43110@gmail.com>
2025-08-22 18:14:25 +0800

    finished the bug memory problem

--- src/lib/Cpu/instructions/Jumps_And _Subroutine_Instructions.ts
+++ src/lib/Cpu/instructions/Jumps_And _Subroutine_Instructions.ts
@@ -5,316 +5,318 @@ import type { Cpu_Register_16Bit } from '../CPU_Register';
 
 // CORRECT
 function CALLN16() {
-  return [
-    // M2
-    (dmg: Gameboy) => {
-      dmg.registers.pointers.PC.increment();
+	return [
+		// M2
+		(dmg: Gameboy) => {
+			dmg.registers.pointers.PC.increment();
 
-      const n = dmg.ram.getMemoryAt(dmg.registers.pointers.PC.getRegister());
+			const n = dmg.ram.getMemoryAt(dmg.registers.pointers.PC.getRegister());
 
-      dmg.registers.setLowerByte(n);
-    },
-    // M3
-    (dmg: Gameboy) => {
-      dmg.registers.pointers.PC.increment();
+			dmg.registers.setLowerByte(n);
+		},
+		// M3
+		(dmg: Gameboy) => {
+			dmg.registers.pointers.PC.increment();
 
-      const n = dmg.ram.getMemoryAt(dmg.registers.pointers.PC.getRegister());
+			const n = dmg.ram.getMemoryAt(dmg.registers.pointers.PC.getRegister());
 
-      dmg.registers.setUpperByte(n);
-    },
-    // M4
-    (dmg: Gameboy) => {
-      dmg.registers.pointers.SP.decrement();
-    }, // M5
-    (dmg: Gameboy) => {
-      dmg.ram.setMemoryAt(
-        dmg.registers.pointers.SP.getRegister(),
-        (dmg.registers.pointers.PC.getRegister() >> 8) & 0xff
-      );
-      dmg.registers.pointers.SP.decrement();
-    },
-    //M6
-    (dmg: Gameboy) => {
-      dmg.ram.setMemoryAt(
-        dmg.registers.pointers.SP.getRegister(),
-        dmg.registers.pointers.PC.getRegister() & 0xff
-      );
-      dmg.registers.pointers.PC.setRegister(
-        (dmg.registers.getUpperByte() << 8) |
-          (dmg.registers.getLowerByte() & 0xff)
-      );
-    },
-    // M7/1
-    (dmg: Gameboy) => {
-      console.log('CALL FINISHED');
-    },
-  ];
+			dmg.registers.setUpperByte(n);
+		},
+		// M4
+		(dmg: Gameboy) => {
+			dmg.registers.pointers.SP.decrement();
+		}, // M5
+		(dmg: Gameboy) => {
+			dmg.ram.setMemoryAt(
+				dmg.registers.pointers.SP.getRegister(),
+				(dmg.registers.pointers.PC.getRegister() >> 8) & 0xff
+			);
+			dmg.registers.pointers.SP.decrement();
+		},
+		//M6
+		(dmg: Gameboy) => {
+			dmg.ram.setMemoryAt(
+				dmg.registers.pointers.SP.getRegister(),
+				dmg.registers.pointers.PC.getRegister() & 0xff
+			);
+			dmg.registers.pointers.PC.setRegister(
+				(dmg.registers.getUpperByte() << 8) |
+					(dmg.registers.getLowerByte() & 0xff)
+			);
+		},
+		// M7/1
+		(dmg: Gameboy) => {
+			console.log('CALL FINISHED');
+		},
+	];
 }
+// needs test
 function CALLCCN16() {
-  return [
-    // M2
-    (dmg: Gameboy) => {
-      dmg.registers.pointers.PC.increment();
+	return [
+		// M2
+		(dmg: Gameboy) => {
+			dmg.registers.pointers.PC.increment();
 
-      const n = dmg.ram.getMemoryAt(dmg.registers.pointers.PC.getRegister());
+			const n = dmg.ram.getMemoryAt(dmg.registers.pointers.PC.getRegister());
 
-      dmg.registers.setLowerByte(n);
-    },
-    // M3
-    (dmg: Gameboy) => {
-      dmg.registers.pointers.PC.increment();
+			dmg.registers.setLowerByte(n);
+		},
+		// M3
+		(dmg: Gameboy) => {
+			dmg.registers.pointers.PC.increment();
 
-      const n = dmg.ram.getMemoryAt(dmg.registers.pointers.PC.getRegister());
+			const n = dmg.ram.getMemoryAt(dmg.registers.pointers.PC.getRegister());
 
-      dmg.registers.setUpperByte(n);
-    },
-    //   M4/1
-    (dmg: Gameboy) => {
-      if (dmg.registers.HALT_BUG) {
-        dmg.registers.HALT_BUG = false;
-      } else {
-        dmg.registers.pointers.PC.increment();
-      }
-    },
-  ];
+			dmg.registers.setUpperByte(n);
+		},
+		//   M4/1
+		(dmg: Gameboy) => {
+			if (dmg.registers.HALT_BUG) {
+				dmg.registers.HALT_BUG = false;
+			} else {
+				dmg.registers.pointers.PC.increment();
+			}
+		},
+	];
 }
 
 // CORRECT LOGIC
 function JPN16() {
-  return [
-    // M2
-    (dmg: Gameboy) => {
-      dmg.registers.pointers.PC.increment();
-      const n = dmg.ram.getMemoryAt(dmg.registers.pointers.PC.getRegister());
-      console.log('GUMANA KA: ', n);
+	return [
+		// M2
+		(dmg: Gameboy) => {
+			dmg.registers.pointers.PC.increment();
+			const n = dmg.ram.getMemoryAt(dmg.registers.pointers.PC.getRegister());
+			console.log('GUMANA KA: ', n);
 
-      dmg.registers.setLowerByte(n);
-    },
-    // M3
-    (dmg: Gameboy) => {
-      dmg.registers.pointers.PC.increment();
-      const n = dmg.ram.getMemoryAt(dmg.registers.pointers.PC.getRegister());
-      console.log('GUMANA KA: ', n);
+			dmg.registers.setLowerByte(n);
+		},
+		// M3
+		(dmg: Gameboy) => {
+			dmg.registers.pointers.PC.increment();
+			const n = dmg.ram.getMemoryAt(dmg.registers.pointers.PC.getRegister());
+			console.log('GUMANA KA: ', n);
 
-      dmg.registers.setUpperByte(n);
-    },
-    // M4
-    (dmg: Gameboy) => {
-      const nn =
-        (dmg.registers.getUpperByte() << 8) | dmg.registers.getLowerByte();
-      dmg.registers.pointers.PC.setRegister(nn);
+			dmg.registers.setUpperByte(n);
+		},
+		// M4
+		(dmg: Gameboy) => {
+			const nn =
+				(dmg.registers.getUpperByte() << 8) | dmg.registers.getLowerByte();
+			dmg.registers.pointers.PC.setRegister(nn);
 
-      console.log(
-        'Jumping to  address',
-        (dmg.registers.getUpperByte() << 8) | dmg.registers.getLowerByte()
-      );
-    },
-    (dmg: Gameboy) => {},
-  ];
+			console.log(
+				'Jumping to  address',
+				(dmg.registers.getUpperByte() << 8) | dmg.registers.getLowerByte()
+			);
+		},
+		(dmg: Gameboy) => {},
+	];
 }
 
 function JPCCN16() {
-  return [
-    // M2
-    (dmg: Gameboy) => {
-      dmg.registers.pointers.PC.increment();
+	return [
+		// M2
+		(dmg: Gameboy) => {
+			dmg.registers.pointers.PC.increment();
 
-      const n = dmg.ram.getMemoryAt(dmg.registers.pointers.PC.getRegister());
+			const n = dmg.ram.getMemoryAt(dmg.registers.pointers.PC.getRegister());
 
-      dmg.registers.setLowerByte(n);
-    },
-    // M3
-    (dmg: Gameboy) => {
-      dmg.registers.pointers.PC.increment();
+			dmg.registers.setLowerByte(n);
+		},
+		// M3
+		(dmg: Gameboy) => {
+			dmg.registers.pointers.PC.increment();
 
-      const n = dmg.ram.getMemoryAt(dmg.registers.pointers.PC.getRegister());
+			const n = dmg.ram.getMemoryAt(dmg.registers.pointers.PC.getRegister());
 
-      dmg.registers.setUpperByte(n);
-    },
-    //   M4/1
-    (dmg: Gameboy) => {
-      if (dmg.registers.HALT_BUG) {
-        dmg.registers.HALT_BUG = false;
-      } else {
-        dmg.registers.pointers.PC.increment();
-      }
-    },
-  ];
+			dmg.registers.setUpperByte(n);
+		},
+		//   M4/1
+		(dmg: Gameboy) => {
+			if (dmg.registers.HALT_BUG) {
+				dmg.registers.HALT_BUG = false;
+			} else {
+				dmg.registers.pointers.PC.increment();
+			}
+		},
+	];
 }
 
+// need test
 function JPHL(HL: Cpu_Register_16Bit<'HL'>, PC: Program_Counter_Register) {
-  PC.setRegister(HL.getRegister());
+	PC.setRegister(HL.getRegister());
 }
-// CORRECT
+// need test
 function RET() {
-  return [
-    (dmg: Gameboy) => {
-      const n = dmg.ram.getMemoryAt(dmg.registers.pointers.SP.getRegister());
-      dmg.registers.setLowerByte(n);
-      dmg.registers.pointers.SP.increment();
-    },
-    (dmg: Gameboy) => {
-      const n = dmg.ram.getMemoryAt(dmg.registers.pointers.SP.getRegister());
-      dmg.registers.setUpperByte(n);
-      dmg.registers.pointers.SP.increment();
-    },
-    (dmg: Gameboy) => {
-      const nn =
-        dmg.registers.getLowerByte() | (dmg.registers.getUpperByte() << 8);
-      dmg.registers.pointers.PC.setRegister(nn);
-    },
-    (dmg: Gameboy) => {
-      console.log('RET PROTOCOL FINISHED');
-    },
-  ];
+	return [
+		(dmg: Gameboy) => {
+			const n = dmg.ram.getMemoryAt(dmg.registers.pointers.SP.getRegister());
+			dmg.registers.setLowerByte(n);
+			dmg.registers.pointers.SP.increment();
+		},
+		(dmg: Gameboy) => {
+			const n = dmg.ram.getMemoryAt(dmg.registers.pointers.SP.getRegister());
+			dmg.registers.setUpperByte(n);
+			dmg.registers.pointers.SP.increment();
+		},
+		(dmg: Gameboy) => {
+			const nn =
+				dmg.registers.getLowerByte() | (dmg.registers.getUpperByte() << 8);
+			dmg.registers.pointers.PC.setRegister(nn);
+		},
+		(dmg: Gameboy) => {
+			console.log('RET PROTOCOL FINISHED');
+		},
+	];
 }
 
 function RETI() {
-  return [
-    (dmg: Gameboy) => {
-      const n = dmg.ram.getMemoryAt(dmg.registers.pointers.SP.getRegister());
-      dmg.registers.setLowerByte(n);
-      dmg.registers.pointers.SP.increment();
-    },
-    (dmg: Gameboy) => {
-      const n = dmg.ram.getMemoryAt(dmg.registers.pointers.SP.getRegister());
-      dmg.registers.setUpperByte(n);
-      dmg.registers.pointers.SP.increment();
-    },
-    (dmg: Gameboy) => {
-      const nn =
-        dmg.registers.getLowerByte() | (dmg.registers.getUpperByte() << 8);
-      dmg.registers.pointers.PC.setRegister(nn);
-      dmg.registers.IME.raiseFlag();
-    },
-    (dmg: Gameboy) => {
-      console.log('RET PROTOCOL FINISHED');
-    },
-  ];
+	return [
+		(dmg: Gameboy) => {
+			const n = dmg.ram.getMemoryAt(dmg.registers.pointers.SP.getRegister());
+			dmg.registers.setLowerByte(n);
+			dmg.registers.pointers.SP.increment();
+		},
+		(dmg: Gameboy) => {
+			const n = dmg.ram.getMemoryAt(dmg.registers.pointers.SP.getRegister());
+			dmg.registers.setUpperByte(n);
+			dmg.registers.pointers.SP.increment();
+		},
+		(dmg: Gameboy) => {
+			const nn =
+				dmg.registers.getLowerByte() | (dmg.registers.getUpperByte() << 8);
+			dmg.registers.pointers.PC.setRegister(nn);
+			dmg.registers.IME.raiseFlag();
+		},
+		(dmg: Gameboy) => {
+			console.log('RET PROTOCOL FINISHED');
+		},
+	];
 }
 
 function RETCC() {
-  return [
-    (dmg: Gameboy) => {
-      console.log('Current RET CC check is false moving to next opcode');
-    },
-    (dmg: Gameboy) => {
-      if (dmg.registers.HALT_BUG) {
-        dmg.registers.HALT_BUG = false;
-      } else {
-        dmg.registers.pointers.PC.increment();
-      }
-    },
-  ];
+	return [
+		(dmg: Gameboy) => {
+			console.log('Current RET CC check is false moving to next opcode');
+		},
+		(dmg: Gameboy) => {
+			if (dmg.registers.HALT_BUG) {
+				dmg.registers.HALT_BUG = false;
+			} else {
+				dmg.registers.pointers.PC.increment();
+			}
+		},
+	];
 }
 // CORRECT
 function RSTN(n: number) {
-  return [
-    // M2
-    (dmg: Gameboy) => {
-      dmg.registers.pointers.SP.decrement();
-      console.log('N IN RSTN', n);
-    },
-    // M3
-    (dmg: Gameboy) => {
-      const msb = dmg.registers.pointers.PC.getRegister() >>> 8;
-      dmg.ram.setMemoryAt(dmg.registers.pointers.SP.getRegister(), msb);
-      dmg.registers.pointers.SP.decrement();
-    },
-    // M4
-    (dmg: Gameboy) => {
-      const lsb = dmg.registers.pointers.PC.getRegister() & 0xff;
-      dmg.ram.setMemoryAt(dmg.registers.pointers.SP.getRegister(), lsb);
-      dmg.registers.pointers.PC.setRegister(n);
-    },
-    // M5
-    (dmg: Gameboy) => {
-      console.log('RST Protocol Finished');
-    },
-  ];
+	return [
+		// M2
+		(dmg: Gameboy) => {
+			dmg.registers.pointers.SP.decrement();
+			console.log('N IN RSTN', n);
+		},
+		// M3
+		(dmg: Gameboy) => {
+			const msb = dmg.registers.pointers.PC.getRegister() >>> 8;
+			dmg.ram.setMemoryAt(dmg.registers.pointers.SP.getRegister(), msb);
+			dmg.registers.pointers.SP.decrement();
+		},
+		// M4
+		(dmg: Gameboy) => {
+			const lsb = dmg.registers.pointers.PC.getRegister() & 0xff;
+			dmg.ram.setMemoryAt(dmg.registers.pointers.SP.getRegister(), lsb);
+			dmg.registers.pointers.PC.setRegister(n);
+		},
+		// M5
+		(dmg: Gameboy) => {
+			console.log('RST Protocol Finished');
+		},
+	];
 }
 // CORRECT - finally
 function JRE() {
-  return [
-    (dmg: Gameboy) => {
-      console.log('CC IS TRUE JRE WILL BE EXECUTED');
-      dmg.registers.pointers.PC.increment();
-    },
-    (dmg: Gameboy) => {
-      // Get current PC and increment to operand
-      let e = dmg.ram.getMemoryAt(dmg.registers.pointers.PC.getRegister());
+	return [
+		(dmg: Gameboy) => {
+			console.log('CC IS TRUE JRE WILL BE EXECUTED');
+			dmg.registers.pointers.PC.increment();
+		},
+		(dmg: Gameboy) => {
+			// Get current PC and increment to operand
+			let e = dmg.ram.getMemoryAt(dmg.registers.pointers.PC.getRegister());
 
-      // Convert to signed 8-bit and add to PC+1
-      if (e > 127) e -= 256;
-      const newPC = (dmg.registers.pointers.PC.getRegister() + e) & 0xffff;
-      dmg.registers.pointers.PC.increment();
+			// Convert to signed 8-bit and add to PC+1
+			if (e > 127) e -= 256;
+			const newPC = (dmg.registers.pointers.PC.getRegister() + e) & 0xffff;
+			dmg.registers.pointers.PC.increment();
 
-      dmg.registers.pointers.PC.setRegister(newPC + 1);
-      console.log('Jumping at address: ', newPC + 1);
-    },
-    (dmg: Gameboy) => {
-      // dmg.registers.pointers.PC.setRegister(dmg.registers.getTempByte());
-    },
-  ];
+			dmg.registers.pointers.PC.setRegister(newPC + 1);
+			console.log('Jumping at address: ', newPC + 1);
+		},
+		(dmg: Gameboy) => {
+			// dmg.registers.pointers.PC.setRegister(dmg.registers.getTempByte());
+		},
+	];
 
-  //   return [
-  //     (dmg: Gameboy) => {
-  //       console.log('CC IS TRUE JRE WILL BE EXECUTED ');
-  //       dmg.registers.pointers.PC.increment();
-  //       const z =
-  //         dmg.ram.getMemoryAt(dmg.registers.pointers.PC.getRegister());
+	//   return [
+	//     (dmg: Gameboy) => {
+	//       console.log('CC IS TRUE JRE WILL BE EXECUTED ');
+	//       dmg.registers.pointers.PC.increment();
+	//       const z =
+	//         dmg.ram.getMemoryAt(dmg.registers.pointers.PC.getRegister());
 
-  //       dmg.registers.setTempByte(z);
-  //     },
-  //     (dmg: Gameboy) => {
-  //       const z = dmg.registers.getTempByte();
-  //       const zSign = z >>> 7 != 0;
-  //       const lsb = dmg.registers.pointers.PC.getRegister() & 0xff;
-  //       const msb = dmg.registers.pointers.PC.getRegister() >>> 8;
+	//       dmg.registers.setTempByte(z);
+	//     },
+	//     (dmg: Gameboy) => {
+	//       const z = dmg.registers.getTempByte();
+	//       const zSign = z >>> 7 != 0;
+	//       const lsb = dmg.registers.pointers.PC.getRegister() & 0xff;
+	//       const msb = dmg.registers.pointers.PC.getRegister() >>> 8;
 
-  //       const carryBit = z + lsb > 0xff;
-  //       const result = (z + lsb) & 0xff;
-  //       // this shit looks like ass
-  //       let adj =
-  //         carryBit && !zSign == true ? 1 : !carryBit && zSign == true ? -1 : 0;
-  //       dmg.registers.setLowerByte(result);
-  //       dmg.registers.setUpperByte(msb + adj);
-  //       dmg.registers.setTempByte(
-  //         (dmg.registers.getUpperByte() << 8) | dmg.registers.getLowerByte()
-  //       );
-  //     },
-  //     (dmg: Gameboy) => {
-  //       dmg.registers.pointers.PC.setRegister(dmg.registers.getTempByte());
-  //     },
-  //   ];
+	//       const carryBit = z + lsb > 0xff;
+	//       const result = (z + lsb) & 0xff;
+	//       // this shit looks like ass
+	//       let adj =
+	//         carryBit && !zSign == true ? 1 : !carryBit && zSign == true ? -1 : 0;
+	//       dmg.registers.setLowerByte(result);
+	//       dmg.registers.setUpperByte(msb + adj);
+	//       dmg.registers.setTempByte(
+	//         (dmg.registers.getUpperByte() << 8) | dmg.registers.getLowerByte()
+	//       );
+	//     },
+	//     (dmg: Gameboy) => {
+	//       dmg.registers.pointers.PC.setRegister(dmg.registers.getTempByte());
+	//     },
+	//   ];
 }
 
 function JREFALSE() {
-  return [
-    () => {
-      console.log('CC IS FALSE JRE WILL NOT BE EXECUTED ');
-    },
-    (dmg: Gameboy) => {
-      if (dmg.registers.HALT_BUG) {
-        dmg.registers.HALT_BUG = false;
-      } else {
-        dmg.registers.pointers.PC.increment();
-      }
-      dmg.registers.pointers.PC.increment();
-    },
-  ];
+	return [
+		() => {
+			console.log('CC IS FALSE JRE WILL NOT BE EXECUTED ');
+		},
+		(dmg: Gameboy) => {
+			if (dmg.registers.HALT_BUG) {
+				dmg.registers.HALT_BUG = false;
+			} else {
+				dmg.registers.pointers.PC.increment();
+			}
+			dmg.registers.pointers.PC.increment();
+		},
+	];
 }
 
 export {
-  CALLN16,
-  CALLCCN16,
-  JPN16,
-  JPCCN16,
-  RET,
-  JPHL,
-  RETCC,
-  RSTN,
-  RETI,
-  JREFALSE,
-  JRE,
+	CALLN16,
+	CALLCCN16,
+	JPN16,
+	JPCCN16,
+	RET,
+	JPHL,
+	RETCC,
+	RSTN,
+	RETI,
+	JREFALSE,
+	JRE,
 };
--- src/lib/Ram/Ram.ts
+++ src/lib/Ram/Ram.ts
@@ -3,47 +3,49 @@
 // making some addresses readonly prolly make this some sort of hook
 
 export class Ram {
-  protected memory: Uint8Array;
+	protected memory: Uint8Array;
 
-  constructor() {
-    this.memory = new Uint8Array(0x10000);
-    this.memory.fill(0);
-  }
-  // is hardware controlled
-  setIE() {}
+	constructor() {
+		this.memory = new Uint8Array(0x10000);
+		this.memory.fill(0);
+	}
+	// is hardware controlled
+	setIE() {}
 
-  getIE() {
-    return this.memory[0xffff];
-  }
-  getIF() {
-    return this.memory[0xff0f];
-  }
+	getIE() {
+		return this.memory[0xffff];
+	}
+	getIF() {
+		return this.memory[0xff0f];
+	}
 
-  isAllowedToInterrupt() {
-    return (this.getIE() & this.getIF()) != 0;
-  }
+	isAllowedToInterrupt() {
+		return (this.getIE() & this.getIF()) != 0;
+	}
 
-  stopValidation() {
-    return ((this.getIE() && 0b1_0000) & (this.getIF() && 0b1_0000)) != 0;
-  }
+	stopValidation() {
+		return ((this.getIE() && 0b1_0000) & (this.getIF() && 0b1_0000)) != 0;
+	}
 
-  getMemory() {
-    return this.memory;
-  }
+	getMemory() {
+		return this.memory;
+	}
 
-  getMemoryAt(index: number) {
-    if (this.memory[index] == undefined) throw new Error('INVALID VALUE');
+	getMemoryAt(index: number) {
+		if (this.memory[index] == undefined) throw new Error('INVALID VALUE');
 
-    return this.memory[index];
-  }
+		return this.memory[index];
+	}
 
-  setMemoryAt(pointer: number, value: number) {
-    this.memory[pointer] = value & 0xff;
-  }
+	setMemoryAt(pointer: number, value: number) {
+		console.log('Wrote at address: ' + pointer + ' wit the value of: ' + value);
 
-  copyROM(raw: Uint8Array) {
-    raw.forEach((value, index) => {
-      this.setMemoryAt(index, value);
-    });
-  }
+		this.memory[pointer] = value & 0xff;
+	}
+
+	copyROM(raw: Uint8Array) {
+		raw.forEach((value, index) => {
+			this.setMemoryAt(index, value);
+		});
+	}
 }
