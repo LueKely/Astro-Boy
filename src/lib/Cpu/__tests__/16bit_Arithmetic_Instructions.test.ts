5808d3f88d97fc99ddfa0d4cb799c9e7a5cb34b8
Lue Kely <luekely43110@gmail.com>
2025-08-25 09:29:12 +0800

    added test for ADD HL ,SP

--- src/lib/Cpu/__tests__/16bit_Arithmetic_Instructions.test.ts
+++ src/lib/Cpu/__tests__/16bit_Arithmetic_Instructions.test.ts
@@ -1,13 +1,13 @@
-import { describe, test, expect } from "vitest";
-import { Cpu_Register_File } from "../CPU_Register_File";
+import { describe, test, expect } from 'vitest';
+import { Cpu_Register_File } from '../CPU_Register_File';
 import {
   ADDHLR16,
   DECR16,
   INCR16,
-} from "../instructions/16bit_Arithmetic_Instructions";
+} from '../instructions/16bit_Arithmetic_Instructions';
 
-describe("ADD HL, R16 Functionalities", () => {
-  test("The sum of the opearation should be 2_082 and no flags should be raised", () => {
+describe('ADD HL, R16 Functionalities', () => {
+  test('The sum of the opearation should be 2_082 and no flags should be raised', () => {
     const CPU = new Cpu_Register_File();
     const { BC: r16 } = CPU.register16Bit;
     r16.setRegister(1_041);
@@ -21,7 +21,7 @@ describe("ADD HL, R16 Functionalities", () => {
     expect(CPU.register.F.getCYFlag()).toBe(0);
   });
 
-  test("Sum should be 4_096 and should raise the HF Flag", () => {
+  test('Sum should be 4_096 and should raise the HF Flag', () => {
     const CPU = new Cpu_Register_File();
 
     const { DE: r16 } = CPU.register16Bit;
@@ -37,7 +37,7 @@ describe("ADD HL, R16 Functionalities", () => {
     expect(CPU.register.F.getCYFlag()).toBe(0);
   });
 
-  test("Sum of the opperation should raise both the HF and CY flag", () => {
+  test('Sum of the opperation should raise both the HF and CY flag', () => {
     const CPU = new Cpu_Register_File();
     const { DE: r16 } = CPU.register16Bit;
     r16.setRegister(0xffff);
@@ -51,10 +51,25 @@ describe("ADD HL, R16 Functionalities", () => {
     expect(CPU.register.F.getHFlag()).toBe(1);
     expect(CPU.register.F.getCYFlag()).toBe(1);
   });
+
+  test('Sum of the opperation should raise both the HF and CY flag', () => {
+    const CPU = new Cpu_Register_File();
+    const { SP: r16 } = CPU.pointers;
+    r16.setRegister(0xffff);
+
+    CPU.register16Bit.HL.setRegister(1);
+
+    ADDHLR16(r16, CPU.register16Bit.HL, CPU.register.F);
+    expect(CPU.register16Bit.HL.getRegister()).toBe(0);
+
+    expect(CPU.register.F.getNFlag()).toBe(0);
+    expect(CPU.register.F.getHFlag()).toBe(1);
+    expect(CPU.register.F.getCYFlag()).toBe(1);
+  });
 });
 
-describe("INC r16 functionalities", () => {
-  test("The value of r16 should increment to 1", () => {
+describe('INC r16 functionalities', () => {
+  test('The value of r16 should increment to 1', () => {
     const CPU = new Cpu_Register_File();
 
     const { BC } = CPU.register16Bit;
@@ -63,8 +78,8 @@ describe("INC r16 functionalities", () => {
     expect(BC.getRegister()).toBe(0b0001_0000_0000_0001);
   });
 });
-describe("DEC r16 functionalities", () => {
-  test("The value of r16 should decrement to 1", () => {
+describe('DEC r16 functionalities', () => {
+  test('The value of r16 should decrement to 1', () => {
     const CPU = new Cpu_Register_File();
 
     const { BC } = CPU.register16Bit;
