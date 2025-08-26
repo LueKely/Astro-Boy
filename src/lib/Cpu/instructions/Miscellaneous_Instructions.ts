8cd70cd1649d9ff66a4fdf3e8006362265eeaa07
Lue Kely <luekely43110@gmail.com>
2025-08-26 11:55:00 +0800

    removed the validator, it kinda looks stupid

--- src/lib/Cpu/instructions/Miscellaneous_Instructions.ts
+++ src/lib/Cpu/instructions/Miscellaneous_Instructions.ts
@@ -3,13 +3,12 @@ import type { Cpu_Register } from '../CPU_Register';
 // TODO: Test all
 function DAA(A: Cpu_Register<'A'>, F: Cpu_Flag_Register) {
   let adjustments = 0;
-  let shouldSetCarry = false;
 
   if (F.getHFlag() || (!F.getNFlag() && (A.getRegister() & 0xf) > 0x9))
     adjustments += 0x6;
   if (F.getCYFlag() || (!F.getNFlag() && A.getRegister() > 0x99)) {
     adjustments += 0x60;
-    shouldSetCarry = true;
+    F.setCYFlag();
   }
 
   adjustments = F.getNFlag() ? -adjustments : adjustments;
@@ -21,10 +20,6 @@ function DAA(A: Cpu_Register<'A'>, F: Cpu_Flag_Register) {
     F.setZFlag();
   } else {
     F.clearZFlag();
-  }
-
-  if (shouldSetCarry) {
-    F.setCYFlag();
   }
 }
 
