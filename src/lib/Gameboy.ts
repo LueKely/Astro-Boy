a1b2690cc6e439d4ff4a736702c3262f8b033fb6
Lue Kely <luekely43110@gmail.com>
2025-08-28 21:30:38 +0800

    find bugs on LD this time

--- src/lib/Gameboy.ts
+++ src/lib/Gameboy.ts
@@ -4,72 +4,72 @@ import { Cpu_Register_File } from './Cpu/CPU_Register_File';
 import { Cpu_Scheduler } from './Cpu/CPU_Scheduler';
 
 export class Gameboy {
-  readonly registers: Cpu_Register_File;
-  readonly ram: Ram;
-  readonly cartridge: GameBoyCatridge;
-  readonly scheduler: Cpu_Scheduler;
-  pause = false;
-  // TODO create a class for timer
-  cycleBudget = 0;
-  lastTime = 0;
+	readonly registers: Cpu_Register_File;
+	readonly ram: Ram;
+	readonly cartridge: GameBoyCatridge;
+	readonly scheduler: Cpu_Scheduler;
+	pause = false;
+	// TODO create a class for timer
+	cycleBudget = 0;
+	lastTime = 0;
 
-  constructor(game: ArrayBuffer) {
-    this.registers = new Cpu_Register_File();
-    this.ram = new Ram();
-    this.cartridge = new GameBoyCatridge(game);
-    this.ram.copyROM(this.cartridge.CartDataToBytes);
-    this.scheduler = new Cpu_Scheduler(this);
-    // timer
-    this.cycleBudget = 0;
-    this.lastTime = performance.now();
-  }
+	constructor(game: ArrayBuffer) {
+		this.registers = new Cpu_Register_File();
+		this.ram = new Ram();
+		this.cartridge = new GameBoyCatridge(game);
+		this.ram.copyROM(this.cartridge.CartDataToBytes);
+		this.scheduler = new Cpu_Scheduler(this);
+		// timer
+		this.cycleBudget = 0;
+		this.lastTime = performance.now();
+	}
 
-  log() {
-    const systemState = {
-      'Program Counter': this.registers.pointers.PC.getRegister(),
-      'Stack Pointer': this.registers.pointers.SP.getRegister(),
-      'Register A': this.registers.register.A.getRegister(),
-      'Register B': this.registers.register.B.getRegister(),
-      'Register C': this.registers.register.C.getRegister(),
-      'Register D': this.registers.register.D.getRegister(),
-      'Register E': this.registers.register.E.getRegister(),
-      'Register H': this.registers.register.H.getRegister(),
-      'Register L': this.registers.register.L.getRegister(),
-      'Register F': this.registers.register.F.getRegister().toString(2),
-      TempByte: this.registers.getTempByte(),
-      LowerByte: this.registers.getLowerByte(),
-      UpperByte: this.registers.getUpperByte(),
+	log() {
+		const systemState = {
+			'Program Counter': this.registers.pointers.PC.getRegister(),
+			'Stack Pointer': this.registers.pointers.SP.getRegister(),
+			'Register A': this.registers.register.A.getRegister(),
+			'Register B': this.registers.register.B.getRegister(),
+			'Register C': this.registers.register.C.getRegister(),
+			'Register D': this.registers.register.D.getRegister(),
+			'Register E': this.registers.register.E.getRegister(),
+			'Register H': this.registers.register.H.getRegister(),
+			'Register L': this.registers.register.L.getRegister(),
+			'Register F': this.registers.register.F.getRegister().toString(2),
+			TempByte: this.registers.getTempByte(),
+			LowerByte: this.registers.getLowerByte(),
+			UpperByte: this.registers.getUpperByte(),
 
-      // IME: this.registers.IME.getValue(),
-      // IF: this.ram.getIF(),
-      // IE: this.ram.getIE(),
-      // HALT_BUG: this.registers.HALT_BUG,
-      // HALT: this.registers.HALT,
-      // STOP: this.registers.STOP,
-      'Memory 0xFF01': this.ram.getMemoryAt(0xff01),
-      'Memory 0xFF02': this.ram.getMemoryAt(0xff02),
-    };
-    console.table(systemState);
-  }
+			// IME: this.registers.IME.getValue(),
+			// IF: this.ram.getIF(),
+			// IE: this.ram.getIE(),
+			// HALT_BUG: this.registers.HALT_BUG,
+			// HALT: this.registers.HALT,
+			// STOP: this.registers.STOP,
+			'Memory 0xFF01': this.ram.getMemoryAt(0xff01),
+			'Memory 0xFF02': this.ram.getMemoryAt(0xff02),
+		};
+		console.table(systemState);
+	}
 
-  stop() {
-    this.pause = !this.pause;
-  }
+	stop() {
+		this.pause = !this.pause;
+	}
 
-  run() {
-    if (this.pause) return;
+	run() {
+		if (this.pause) return;
 
-    // for (let i = 0; i < 50000; i++) {
-    //   this.scheduler.tick();
-    // }
+		// for (let i = 0; i < 50000; i++) {
+		//   this.scheduler.tick();
+		// }
 
-    // requestAnimationFrame(() => this.run());
+		// requestAnimationFrame(() => this.run());
 
-    for (let i = 0; i < 41_000; i++) {
-      this.scheduler.tick();
-    }
-    // console.log(this.ram.getMemory()[49700]);
-    // console.log(this.ram.getMemory()[49701]);
-    // console.log(this.ram.getMemory()[49702]);
-  }
+		for (let i = 0; i < 41_000; i++) {
+			this.scheduler.tick();
+		}
+		// console.log(this.ram.getMemory()[49700]);
+		// console.log(this.ram.getMemory()[49701]);
+		// console.log(this.ram.getMemory()[49702]);
+	}
 }
