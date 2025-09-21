39ebcac885fa531c4bac75c3113af741559dfe9b
Lue Kely <luekely43110@gmail.com>
2025-09-21 09:45:04 +0800

    i think this is pretty okay

--- src/lib/Ppu/Canvas.ts
+++ src/lib/Ppu/Canvas.ts
@@ -1,19 +1,18 @@
 export class Canvas {
-  canvas: HTMLCanvasElement;
-  ctx: CanvasRenderingContext2D | null;
+    canvas: HTMLCanvasElement;
+    ctx: CanvasRenderingContext2D | null;
 
-  //   seems like drawing this canvas is a bit weird
-  //   and pixels for 144x160 is tad small i need to scale this bigger!
-  constructor(canvas: HTMLCanvasElement | null) {
-    if (canvas) {
-      this.canvas = canvas;
-      this.ctx = this.canvas.getContext('2d');
-
-      console.log('Canvas Initialized');
-    } else {
-      throw Error('Canvas Does Not Exist M8');
+    //   seems like drawing this canvas is a bit weird
+    //   and pixels for 144x160 is tad small i need to scale this bigger!
+    constructor(canvas: HTMLCanvasElement | null) {
+        if (canvas) {
+            this.canvas = canvas;
+            this.ctx = this.canvas.getContext('2d');
+            console.log('Canvas Initialized');
+        } else {
+            throw Error('Canvas Does Not Exist M8');
+        }
     }
-  }
 
-  draw() {}
+    draw() {}
 }
