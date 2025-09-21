1ed387c3aefc71bfc6aaecd08bfc5817e36cdf10
Lue Kely <luekely43110@gmail.com>
2025-09-21 09:39:08 +0800

    added prettier config

---
+++ src/lib/Ppu/Canvas.ts
@@ -0,0 +1,19 @@
+export class Canvas {
+  canvas: HTMLCanvasElement;
+  ctx: CanvasRenderingContext2D | null;
+
+  //   seems like drawing this canvas is a bit weird
+  //   and pixels for 144x160 is tad small i need to scale this bigger!
+  constructor(canvas: HTMLCanvasElement | null) {
+    if (canvas) {
+      this.canvas = canvas;
+      this.ctx = this.canvas.getContext('2d');
+
+      console.log('Canvas Initialized');
+    } else {
+      throw Error('Canvas Does Not Exist M8');
+    }
+  }
+
+  draw() {}
+}
--- src/lib/Ppu/PPU.ts
+++ src/lib/Ppu/PPU.ts
@@ -1,53 +1,54 @@
 export class PPU {
-  // this should store 384 tiles
-  tileCache: number[][][] = [];
+	// this should store 384 tiles
+	tileCache: number[][][] = [];
 
-  static twoBitPixelProcessing(lsb: number, msb: number): number[] {
-    const result = [];
-    for (let index = 7; index >= 0; index--) {
-      const mask = 0b0000_0001 << index;
-      const maskLsb = (mask & lsb) >> index;
-      const maskMsb = (mask & msb) >> index;
+	static twoBitPixelProcessing(lsb: number, msb: number): number[] {
+		const result = [];
 
-      if (maskMsb === maskLsb) {
-        if (maskMsb != 0) {
-          result.push(3);
-        } else {
-          result.push(0);
-        }
-      } else if (maskMsb > maskLsb) {
-        result.push(2);
-      } else if (maskMsb < maskLsb) {
-        result.push(1);
-      }
-    }
+		for (let index = 7; index >= 0; index--) {
+			const mask = 0b0000_0001 << index;
+			const maskLsb = (mask & lsb) >> index;
+			const maskMsb = (mask & msb) >> index;
 
-    return result;
-  }
-  // this should take 16 items from an array to crate the 8by8 tile
-  static renderATile(vramSlice: Uint8Array) {
-    const tile: number[][] = [];
-    for (let index = 0; index < vramSlice.length; index += 2) {
-      const lsb = vramSlice[index];
-      const msb = vramSlice[index + 1];
+			if (maskMsb === maskLsb) {
+				if (maskMsb != 0) {
+					result.push(3);
+				} else {
+					result.push(0);
+				}
+			} else if (maskMsb > maskLsb) {
+				result.push(2);
+			} else if (maskMsb < maskLsb) {
+				result.push(1);
+			}
+		}
 
-      const tileRow = PPU.twoBitPixelProcessing(lsb, msb);
-      tile.push(tileRow);
-    }
-    return tile;
-  }
+		return result;
+	}
 
-  static renderTileData(vram: Uint8Array): number[][][] {
-    const cached: number[][][] = [];
-    for (let index = 0; index < vram.length; index += 16) {
-      const tile = PPU.renderATile(vram.slice(0, 15));
-      cached.push(tile);
-    }
-    return cached;
-  }
-  // TILE MAP
-  //Todo:
-  // Canvas Rendering
-  // Canvas should have a 2d grid
-  // Tile map has 3 layers
+	// this should take 16 items from an array to crate the 8x8 tile
+	static renderATile(vramSlice: Uint8Array) {
+		const tile: number[][] = [];
+
+		for (let index = 0; index < vramSlice.length; index += 2) {
+			const lsb = vramSlice[index];
+			const msb = vramSlice[index + 1];
+
+			const tileRow = PPU.twoBitPixelProcessing(lsb, msb);
+			tile.push(tileRow);
+		}
+
+		return tile;
+	}
+
+	static renderTileData(vram: Uint8Array): number[][][] {
+		const cached: number[][][] = [];
+
+		for (let index = 0; index < vram.length; index += 16) {
+			const tile = PPU.renderATile(vram.slice(0, 15));
+			cached.push(tile);
+		}
+
+		return cached;
+	}
 }
