ce9778b476f7bda05e24a8d5932b634afcbb6ae6
Lue Kely <luekely43110@gmail.com>
2025-11-16 13:10:25 +0800

    i locked in

--- src/components/layout/CatridgeLayout.tsx
+++ src/components/layout/CatridgeLayout.tsx
@@ -4,11 +4,11 @@ export function CartridgeLayout() {
     return (
         <div className="cartridge--container">
             <Link name="CPU" link="cpu" />
-            <Link name="Catridge" link="cartridge" />{' '}
-            <div className="wrapper">
+            <Link name="Catridge" link="cartridge" />
+            {/*<div className="wrapper">
                 <input type="file" id="fileInput" />
                 <output id="output">No Games Loaded</output>
-            </div>
+            </div>*/}
         </div>
     );
 }
