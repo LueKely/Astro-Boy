5c86d450b6bfc98b6fd8bd00e1b864e292962cdb
Lue Kely <luekely43110@gmail.com>
2025-11-18 18:41:58 +0800

    added the font and stuff

--- src/components/composables/link.tsx
+++ src/components/composables/link.tsx
@@ -1,5 +1,5 @@
 export function Link({ link, name }: { link: string; name: string }) {
-    const css = `${link}--link  bagel-fat-one-regular `;
+    const css = `${link}--link  bai-jamjuree-regular link  `;
     return (
         <a href={'/' + link} className={css}>
             {name}
