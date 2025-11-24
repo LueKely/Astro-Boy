f1c0dbed4c9c298a9367fb3f5459be4d9f621304
Lue Kely <luekely43110@gmail.com>
2025-11-24 19:36:28 +0800

    added more input

--- src/components/forms/Registers.tsx
+++ src/components/forms/Registers.tsx
@@ -1,4 +1,4 @@
-import { useRef, type FormEventHandler } from 'react';
+import { useRef } from 'react';
 import { FormInput } from '../composables/input';
 export function RegistersForm() {
     const aRegister = useRef<HTMLInputElement>(null);
@@ -7,6 +7,13 @@ export function RegistersForm() {
         <fieldset className="register">
             <legend>Registers</legend>
             <FormInput ref={aRegister} inputName="A"></FormInput>
+            <FormInput ref={aRegister} inputName="F"></FormInput>
+            <FormInput ref={aRegister} inputName="B"></FormInput>
+            <FormInput ref={aRegister} inputName="C"></FormInput>
+            <FormInput ref={aRegister} inputName="D"></FormInput>
+            <FormInput ref={aRegister} inputName="E"></FormInput>
+            <FormInput ref={aRegister} inputName="H"></FormInput>
+            <FormInput ref={aRegister} inputName="L"></FormInput>
         </fieldset>
     );
 }
