fb2f572deb2be6e2044ce75112b2acf0b461527b
Lue Kely <luekely43110@gmail.com>
2025-11-25 17:15:50 +0800

    added more stuff in the forms

--- src/components/forms/Ram.tsx
+++ src/components/forms/Ram.tsx
@@ -1,10 +1,13 @@
 import { FormButton } from '../composables/button';
+import { FormInput } from '../composables/input';
 
 export function RamForm() {
     return (
         <fieldset className="ram">
             <legend>Ram</legend>
-            <FormButton name="Gaming"></FormButton>
+            {/*<FormButton name="Gaming"></FormButton>*/}
+            <FormInput ref={null} styleName={{ width: '200px' }} inputName="Address"></FormInput>
+            <FormInput ref={null} styleName={{ width: '200px' }} inputName="Value"></FormInput>
         </fieldset>
     );
 }
