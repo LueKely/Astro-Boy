bcecf2313903f4dc54c0f4ac00f60a23c13054cb
Lue Kely <luekely43110@gmail.com>
2025-12-11 15:17:03 +0800

    about nalang

--- src/components/forms/StatusWindow.tsx
+++ src/components/forms/StatusWindow.tsx
@@ -2,6 +2,7 @@ export function StatusWindow() {
     return (
         <fieldset className="status">
             <legend>Status Window</legend>
+            <img width={130} height={130} src="/bot.gif"></img>
         </fieldset>
     );
 }
