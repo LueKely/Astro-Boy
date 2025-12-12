2a4c8d3a675ede0bf6dbe69c15a58e4bfe301dff
Lue Kely <luekely43110@gmail.com>
2025-12-12 12:31:12 +0800

    im done with this

--- src/components/forms/StatusWindow.tsx
+++ src/components/forms/StatusWindow.tsx
@@ -1,7 +1,7 @@
 export function StatusWindow() {
     return (
         <fieldset className="status">
-            <legend>Status Window</legend>
+            <legend>Little Guy</legend>
             <img width={130} height={130} src="/bot.gif"></img>
         </fieldset>
     );
